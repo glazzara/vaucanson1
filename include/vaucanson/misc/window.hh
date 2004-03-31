14c84e11738c320ba4839e4e23d4fc030500652d
Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
2004-03-31 21:10:47 +0000

    2003-09-04  Thomas Claveirole  <thomas.claveirole@lrde.epita.fr>
    	* include/vaucanson/misc/window.hxx: New.
    	* include/vaucanson/misc/window.hh: New.
    	* include/Makefile.am: Update.

---
+++ include/vaucanson/misc/window.hh
@@ -0,0 +1,148 @@
+// window.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2001,2002,2003 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//
+#ifndef VCSN_MISC_WINDOW_HH
+# define VCSN_MISC_WINDOW_HH
+
+/**
+ * @file   window.hh
+ * @brief  Window class for regular expression search on streams.
+ * 
+ * @author Thomas Claveirole <thomas@lrde.epita.fr>
+ * @see Window
+ */
+
+# include <iostream>
+# include <string>
+
+namespace utility {
+
+  /** @addtogroup utility *//** @{ */
+
+  /** 
+   * @brief Handle windows in streams for performing regular expression search.
+   *
+   * A window is a portion of a stream, viewable as a string, on which
+   * several operations can be performed:
+   * <ul>
+   *  <li> Read the window's characters. </li>
+   *  <li> Shift the window forward. </li>
+   * </ul>
+   * A window has a constant size, unless an end of line ('\n') or an
+   * end of file occurs, in which case a window is shorter than its
+   * supposed size.
+   * 
+   * @param InputIterator The type of input iterator used to read the stream.
+   *                      It shall be a random access iterator.
+   * @param Letter The type  used for letters.
+   *
+   * @author Thomas Claveirole <thomas@lrde.epita.fr>
+   */
+  template <class InputIterator, class Letter>
+  struct Window
+  {
+    typedef InputIterator				iterator_t;
+    typedef Letter					letter_t;
+    typedef typename std::basic_string<letter_t>	string_t;
+    typedef unsigned int				length_t;
+    
+    /**
+     * @ brief Standard constructor for the Window class.
+     *
+     * Build a window from a couple of begin/end iterator, a newline
+     * character and a window length.
+     *
+     * @param stream Iterator to the begining of the stream.
+     * @param eof Iterator to the end of the stream.
+     * @param eol The newline character.
+     * @param length The window length.
+     *
+     * @see Window
+     */
+    Window(const iterator_t& stream,
+	   const iterator_t& eof,
+	   letter_t eol,
+	   length_t length);
+    
+    /// Indicates wether the end of the stream has been reached or not.
+    bool		eof() const;
+    /// Indicates wether a new line has been reached or not.
+    bool		eol() const;
+
+    /**
+     * @brief Shift the window.
+     *
+     * @params n The number of character the window must be shifted. When
+     *           not specified, the window length is taken.
+     */
+    /// @{
+    void		shift(unsigned int n);
+    void		shift();
+    /// @}
+
+    /// Move to a specific offset.
+    void		moveto(length_t offset);
+
+    /// Returns the actual window size.
+    length_t		size() const;
+
+    /// Returns the i-th character of the window.
+    letter_t		operator [] (length_t i) const;
+
+    /// Print the window.
+    std::ostream&	print(std::ostream& ostr) const;
+
+    /// Accessor to class members.
+    /// @{
+    size_t		offset() const;
+    length_t		length() const;
+    string_t		window() const;
+    /// @}
+  protected:
+    void		compute_size();
+
+    iterator_t		begin_;
+    iterator_t		stream_;
+    iterator_t		end_;
+    letter_t		eol_;
+    length_t		length_;
+    length_t		size_;
+  };
+  
+  template <class InputIterator, class Letter>
+  std::ostream&
+  operator << (std::ostream& ostr, const Window<InputIterator, Letter>& w);
+  
+  /** @} */
+
+} // utility
+
+# include <vaucanson/misc/window.hxx>
+
+#endif // VCSN_MISC_WINDOW_HH
---
+++ include/vaucanson/misc/window.hxx
@@ -0,0 +1,172 @@
+// window.hxx: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2001,2002,2003 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//
+#ifndef VCSN_MISC_WINDOW_HXX
+# define VCSN_MISC_WINDOW_HXX
+
+/**
+ * @file   window.hxx
+ * @brief  Window class for regular expression search on streams.
+ * 
+ * @author Thomas Claveirole <thomas@lrde.epita.fr>
+ * @see Window
+ */
+
+# include <vaucanson/misc/window.hh>
+# include <vaucanson/misc/contract.hh>
+
+namespace utility {
+
+  /** @addtogroup utility *//** @{ */
+  
+  template <class InputIterator, class Letter>
+  Window<InputIterator, Letter>::Window(const iterator_t& stream,
+					const iterator_t& eof,
+					letter_t eol,
+					length_t length) :
+    begin_	(stream),
+    stream_	(stream),
+    end_	(eof),
+    eol_	(eol),
+    length_	(length)
+  {
+    precondition(length > 0);
+    
+    compute_size();
+  }
+      
+  template <class InputIterator, class Letter>
+  bool
+  Window<InputIterator, Letter>::eof() const
+  {
+    return stream_ == end_;
+  }
+  
+  template <class InputIterator, class Letter>
+  bool
+  Window<InputIterator, Letter>::eol() const
+  {
+    return eof() || (*stream_ == eol_);
+  }
+  
+  template <class InputIterator, class Letter>
+  void
+  Window<InputIterator, Letter>::shift(unsigned int n)
+  {
+    precondition(n <= size_);
+    
+    if ((stream_ != end_) && (*stream_ == eol_))
+      ++stream_;
+    else
+      stream_ += n;
+    compute_size();
+  }
+
+  template <class InputIterator, class Letter>
+  void
+  Window<InputIterator, Letter>::shift()
+  {
+    shift(size());
+  }
+
+  template <class InputIterator, class Letter>
+  void
+  Window<InputIterator, Letter>::moveto(length_t offset)
+  {
+    stream_ = begin_ + offset;
+    compute_size();
+  }
+
+  template <class InputIterator, class Letter>
+  typename Window<InputIterator, Letter>::length_t
+  Window<InputIterator, Letter>::size() const
+  {
+    return size_;
+  }
+  
+  template <class InputIterator, class Letter>
+  typename Window<InputIterator, Letter>::letter_t
+  Window<InputIterator, Letter>::operator [] (length_t i) const
+  {
+    precondition (i < size_);
+    
+    return stream_[i];
+  }
+  
+  template <class InputIterator, class Letter>
+  std::ostream&
+  Window<InputIterator, Letter>::print(std::ostream& ostr) const
+  {
+    return ostr << window();
+  }
+
+  template <class InputIterator, class Letter>
+  size_t
+  Window<InputIterator, Letter>::offset() const
+  {
+    return stream_ - begin_;
+  }
+  
+  template <class InputIterator, class Letter>
+  typename Window<InputIterator, Letter>::length_t
+  Window<InputIterator, Letter>::length() const
+  {
+    return length_;
+  }
+
+  template <class InputIterator, class Letter>
+  typename Window<InputIterator, Letter>::string_t
+  Window<InputIterator, Letter>::window() const
+  {
+    return string_t (stream_, stream + size_);
+  }
+
+  template <class InputIterator, class Letter>
+  void
+  Window<InputIterator, Letter>::compute_size()
+  {
+    size_ = 0;
+    while ((size_ < length_) &&
+	   (stream_ + size_ != end_) &&
+	   (stream_[size_] != eol_))
+      ++size_;
+  }
+  
+  template <class InputIterator, class Letter>
+  std::ostream&
+  operator << (std::ostream& ostr, const Window<InputIterator, Letter>& w)
+  {
+    return w.print(ostr);
+  }
+  
+  /** @} */
+  
+} // utility
+
+#endif // VCSN_MISC_WINDOW_HXX
