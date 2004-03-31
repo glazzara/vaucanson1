733fda706a5b7f38d9fee2c1e985c4806265fc4c
prcs2svn <prcs2svn>
2004-03-31 20:09:07 +0000

    Only merge branches, conflicts will be resolved later.

--- include/vaucanson/automata/concept/transducer.hh
+++ include/vaucanson/automata/concept/transducer.hh
@@ -1,5 +1,6 @@
 <<<<<<< .working
 <<<<<<< .working
+<<<<<<< .working
 // transducer.hh: this file is part of the Vaucanson project.
 //
 // Vaucanson, a generic library for finite state machines.
@@ -323,3 +324,118 @@ namespace vcsn {
 
 #endif // VCSN_AUTOMATA_CONCEPT_TRANSDUCER_HH
 >>>>>>> .merge-right.r339
+=======
+// transducer.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2001,2002,2003 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//
+#ifndef VCSN_AUTOMATA_CONCEPT_TRANSDUCER_HH
+# define VCSN_AUTOMATA_CONCEPT_TRANSDUCER_HH
+
+# include <vaucanson/fundamental/fundamental.hh>
+# include <vaucanson/automata/concept/automata.hh>
+# include <vaucanson/automata/concept/automata_base.hh>
+# include <vaucanson/automata/concept/transducer_base.hh>
+
+namespace vcsn {
+
+  template <class Series>
+  struct Transducer;
+  
+   template <class Series>
+   struct dynamic_traits<Transducer<Series> >
+   {
+     static const bool ret = dynamic_traits<Series>::ret;    
+   };
+ 
+  template <class Series, typename T>
+  struct MetaElement<Transducer<Series>, T>
+    : MetaElement<TransducerBase<Transducer<Series> >, T>
+  {};
+
+  template <class Series>
+  struct virtual_types<Transducer<Series> >
+  {
+    typedef Series		series_t;
+  };
+    
+  template <class Series>
+  class Transducer
+    : public TransducerBase<Transducer<Series> >
+  {
+  public:
+    typedef Transducer<Series>				     self_t;
+    typedef typename virtual_types<self_t>::series_t	     series_t;
+
+    Transducer(const series_t&);
+
+    const series_t&	series() const;
+
+  private:
+    series_t	series_;
+  };
+
+  template <class S, class T>
+  struct output_projection_helper
+  {
+    typedef typename S::series_t::weights_t                    
+    typeof_auto_series_t;
+    typedef typename S::series_t::monoid_t              auto_monoid_t;
+    typedef typename typeof_auto_series_t::weights_t           auto_semiring_t;
+    typedef typename algebra::mute_series_traits<typeof_auto_series_t,
+						 auto_semiring_t,
+						 auto_monoid_t>::ret    
+    auto_series_t;
+    
+    typedef typename output_projection_traits<T>::ret          auto_impl_t;
+    typedef Element<Automata<auto_series_t>, 
+		    auto_impl_t>      ret;
+  };
+
+  template <class S, class T>
+  struct identity_transducer_helper
+  {
+    typedef typename S::series_t	 series_t;
+    typedef typename series_t::monoid_t  monoid_t;
+    typedef typename series_t::weights_t weights_t;
+    typedef typename algebra::mute_series_traits<series_t, series_t, monoid_t>
+    ::ret    tseries_t;
+    typedef typename extension_traits<T>::ret impl_t;
+    typedef Element<Transducer<tseries_t>, impl_t> ret;
+  };
+
+  template <class S, class T>
+  typename identity_transducer_helper<S, T>::ret
+  partial_identity(const Element<S, T>&);
+
+} // vcsn
+
+# include <vaucanson/automata/concept/transducer.hxx>
+
+#endif // VCSN_AUTOMATA_CONCEPT_TRANSDUCER_HH
+>>>>>>> .merge-right.r340
--- src/tests/algebra/series/krat/derivations/derivation/krat_exp_derivation_product_test.hh
+++ src/tests/algebra/series/krat/derivations/derivation/krat_exp_derivation_product_test.hh
@@ -1,5 +1,6 @@
 <<<<<<< .working
 <<<<<<< .working
+<<<<<<< .working
 // krat_exp_derivation_product_test.hh: this file is part of the Vaucanson project.
 //
 // Vaucanson, a generic library for finite state machines.
@@ -308,3 +309,106 @@ bool krat_exp_derivation_product_test(tests::Tester& tg)
 
 #endif // VCSN_TESTS_ALGEBRA_SERIES_KRAT_DERIVATIONS_DERIVATION_KRAT_EXP_DERIVATION_PRODUCT_TEST_HH
 >>>>>>> .merge-right.r339
+=======
+// krat_exp_derivation_product_test.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2001,2002,2003 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//
+#ifndef VCSN_TESTS_ALGEBRA_SERIES_KRAT_DERIVATIONS_DERIVATION_KRAT_EXP_DERIVATION_PRODUCT_TEST_HH
+# define VCSN_TESTS_ALGEBRA_SERIES_KRAT_DERIVATIONS_DERIVATION_KRAT_EXP_DERIVATION_PRODUCT_TEST_HH
+
+# include <check/tester.hh>
+# include <vaucanson/tools/gen_random.hh>
+# include <vaucanson/tools/usual.hh>
+# include <vaucanson/fundamental/element.hh>
+# include <vaucanson/algorithms/krat_exp_derivation.hh>
+# include <vaucanson/misc/selectors.hh>
+
+using namespace vcsn;
+
+template <class Expr, class Derivation>
+bool krat_exp_derivation_product_test(tests::Tester& tg)
+{
+  TEST_DERIVATION_TYPEDEF_DECS(Expr, Derivation);
+
+  tests::Tester t(tg.verbose());
+  srand(time(0));
+  TEST_DERIVATION_USUAL_DECS(alphabet, a, b, monoid, semiring, series);
+
+  const int nb_iter = 20;
+  int sucs = 0;
+  int tsts = 0;
+  for (int i = 0; i < nb_iter; ++i)
+    {
+      // 1/ LHS is non-cancelable
+
+      krat_exp_t random = series.choose(SELECT(kexp_t));
+      krat_exp_t krat_a (series, a);
+      krat_exp_t krat_b (series, b);
+      // 0.xyz
+      TEST_DERIVATE(tsts, sucs, zero_as<kexp_t>::of(series) * random, a,
+		    zero_as<kexp_t>::of(series));
+      // a.xyz
+      TEST_DERIVATE(tsts, sucs, krat_a * random, a, random);
+      TEST_DERIVATE(tsts, sucs, krat_a * random, b,
+		    zero_as<kexp_t>::of(series));
+      // (a+b).xyz
+      TEST_DERIVATE(tsts, sucs, (krat_a + krat_b) * random, a, random);
+      TEST_DERIVATE(tsts, sucs, (krat_a + krat_b) * random, b, random);
+      // (a.b).xyz
+      TEST_DERIVATE(tsts, sucs, krat_a * krat_b * random, a, krat_b * random);
+      TEST_DERIVATE(tsts, sucs, krat_a * krat_b * random, b,
+		    zero_as<kexp_t>::of(series));
+
+      // 2/ LHS *is* cancelable
+
+      std::pair<krat_exp_t, krat_exp_t> random_a =
+	choose_exp_and_derivate<derivation_t, krat_exp_t>(series, a);
+      std::pair<krat_exp_t, krat_exp_t> random_b =
+	choose_exp_and_derivate<derivation_t, krat_exp_t>(series, b);
+      std::pair<krat_exp_t, krat_exp_t> cancel_a =
+       choose_cancelable_exp_and_derivate<derivation_t, krat_exp_t>(series, a);
+      std::pair<krat_exp_t, krat_exp_t> cancel_b =
+       choose_cancelable_exp_and_derivate<derivation_t, krat_exp_t>(series, b);
+      // cancelable * random
+      TEST_DERIVATE(tsts, sucs,
+		    cancel_a.first * random_a.first, a,
+		    cancel_a.second * random_a.first + random_a.second);
+      TEST_DERIVATE(tsts, sucs,
+		    cancel_b.first * random_b.first, b,
+		    cancel_b.second * random_b.first + random_b.second);
+    }
+  
+  std::string rate;
+  SUCCESS_RATE(rate, sucs, tsts);
+  TEST(t, "Various derivation tests on a product. " + rate, sucs == tsts);
+  return t.all_passed();
+}
+
+#endif // VCSN_TESTS_ALGEBRA_SERIES_KRAT_DERIVATIONS_DERIVATION_KRAT_EXP_DERIVATION_PRODUCT_TEST_HH
+>>>>>>> .merge-right.r340
