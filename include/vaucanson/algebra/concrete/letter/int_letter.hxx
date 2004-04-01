30ebdc0b653ec469d46fca10d8e0eabb32eb463f
Valentin David <valentin@lrde.epita.fr>
2004-04-01 01:50:31 +0000

    2004-03-21  Valentin David  <valentin@lrde.epita.fr>
    	* include/vaucanson/xml/xml_value.hh,
    	* include/vaucanson/xml/krat_to_polynom.hh,
    	* include/vaucanson/xml/ios.hxx, include/vaucanson/xml/xml_ops.hxx,
    	* include/vaucanson/xml/krat_to_polynom.hxx,
    	* include/vaucanson/xml/error_handler.hxx,
    	* include/vaucanson/xml/defaults.hh, include/vaucanson/xml/ios.hh,
    	* include/vaucanson/xml/error_handler.hh,
    	* include/vaucanson/xml/strings.hh, include/vaucanson/xml/infos.hh,
    	* include/vaucanson/xml/session.hh, include/vaucanson/xml/xml_set.hxx,
    	* include/vaucanson/xml/xml_automaton.hh,
    	* include/vaucanson/xml/strings.hxx,
    	* include/vaucanson/xml/xml_automaton.hxx,
    	* include/vaucanson/xml/xml_loader.hh,
    	* include/vaucanson/xml/callback.hxx, include/vaucanson/xml/xml_set.hh,
    	* include/vaucanson/xml/xml_loader.hxx,
    	* include/vaucanson/xml/xml_ops.hh,
    	* include/vaucanson/xml/xml_value.hxx,
    	* include/vaucanson/xml/session.hxx, include/vaucanson/xml/errors.hh,
    	* include/vaucanson/xml/static.hh, include/vaucanson/xml/types.hh,
    	* include/vaucanson/xml/dynamic.hh, include/vaucanson/xml/generic.hh,
    	* include/vaucanson/xml/callback.hh,
    	* include/vaucanson/algebra/concrete/letter/int_letter.hxx,
    	* include/vaucanson/algebra/concrete/letter/int_letter.hh,
    	* include/vaucanson/algebra/concrete/free_monoid/tools.hh,
    	* include/vaucanson/algebra/concrete/free_monoid/tools.hxx:
    	Add missing copyright headers.

--- include/vaucanson/algebra/concrete/letter/int_letter.hxx
+++ include/vaucanson/algebra/concrete/letter/int_letter.hxx
@@ -1,3 +1,32 @@
+// int_letter.hxx: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2004 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//
 #ifndef VCSN_ALGEBRA_CONCRETE_LETTER_INT_LETTER_HXX
 # define VCSN_ALGEBRA_CONCRETE_LETTER_INT_LETTER_HXX
 
@@ -68,4 +97,4 @@ namespace std {
 
 } // std
 
-#endif // VCSN_ALGEBRA_CONCRETE_LETTER_COUPLE_LETTER_HXX
+#endif // VCSN_ALGEBRA_CONCRETE_LETTER_INT_LETTER_HXX
