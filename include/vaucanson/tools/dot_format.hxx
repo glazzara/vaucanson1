11ca64e7e95e23ea9bf4c13f78ec2a7bf0e678f2
Florent Terrones <florent.terrones@lrde.epita.fr>
2005-05-12 09:55:08 +0000

    2005-05-12  Florent Terrones  <florent.terrones@lrde.epita.fr>
    	* include/vaucanson/tools/dot_format.hxx: Enclose the
    	automaton name with quotes and escape those which are
    	in it, so as to avoid a bug that occured if it contained
    	quotes.

--- include/vaucanson/tools/dot_format.hxx
+++ include/vaucanson/tools/dot_format.hxx
@@ -35,6 +35,7 @@
 # include <sstream>
 # include <map>
 
+# include <vaucanson/misc/escaper.hh>
 # include <vaucanson/tools/dot_format.hh>
 # include <vaucanson/automata/concept/handlers.hh>
 # include <vaucanson/tools/usual_macros.hh>
@@ -44,8 +45,13 @@ namespace vcsn
   namespace io
   {
     inline dot::dot(const std::string& name)
-      : name_(name)
-    {}
+    {
+      std::ostringstream os;
+      std::set<char> to_escape;
+      to_escape.insert('"');
+      (utility::setesc(to_escape))(os) << utility::escaper<std::string>(name);
+      name_ = std::string("\"") + os.str();
+    }
 
     template<typename Saver, typename Conv>
     void dot::operator()(std::ostream& out, const Saver& s,
@@ -53,12 +59,11 @@ namespace vcsn
     {
       typedef typename Saver::automaton_t auto_t;
       const auto_t& a = s.automaton();
-      std::string   name("automaton_name");
       unsigned count = 0;
       std::map<hstate_t, unsigned> state_map;
 
       out << "digraph vcsn {" << std::endl
-	  << "label=\"" << name_ << ' ' << a << "\";" << std::endl
+	  << "label=\"" << name_.c_str() + 1 << ' ' << a << "\";" << std::endl
 	  << "node [shape=circle];" << std::endl;
 
       for (typename auto_t::state_iterator i = a.states().begin();
@@ -68,42 +73,47 @@ namespace vcsn
 	  unsigned c = state_map[*i] = count++;
 	  if (a.is_initial(*i))
 	    {
-	      out << name << count
-		  << " [style=invis,label=\"\",width=.01,height=.01];"
+	      out << name_ << count
+		  << "\" [style=invis,label=\"\",width=.01,height=.01];"
 		  << std::endl
-		  << name << count << " -> " << name << c
-		  << " [label=\"" << conv(a, a.get_initial(*i)) << "\"];"
+		  << name_ << count << "\" -> " << name_ << c
+		  << "\" [label=\"" << conv(a, a.get_initial(*i)) << "\"];"
 		  << std::endl;
 	      ++count;
 	    }
 	  if (a.is_final(*i))
 	    {
-	      out << name << count
-		  << " [style=invis,label=\"\",width=.01,height=.01];"
+	      out << name_ << count
+		  << "\" [style=invis,label=\"\",width=.01,height=.01];"
 		  << std::endl
-		  << name << c << " -> "  << name << count
-		  << " [label=\""<< conv(a, a.get_final(*i)) <<"\"];"
+		  << name_ << c << "\" -> "  << name_ << count
+		  << "\" [label=\""<< conv(a, a.get_final(*i)) <<"\"];"
 		  << std::endl;
 	      ++count;
 	    }
-	  out << name << c << " [label=\"" << *i << "\"];" << std::endl;
+	  out << name_ << c << "\" [label=\"" << *i << "\"];" << std::endl;
 	}
       for (typename auto_t::edge_iterator i = a.edges().begin();
 	   i != a.edges().end();
 	   ++i)
 	{
-	  out << name << state_map[a.origin_of(*i)]
-	      << " -> "
-	      << name << state_map[a.aim_of(*i)];
-	  out << " [label=\"" << conv(a, a.series_of(*i)) << "\"];"
+	  out << name_ << state_map[a.origin_of(*i)]
+	      << "\" -> "
+	      << name_ << state_map[a.aim_of(*i)];
+	  out << "\" [label=\"" << conv(a, a.series_of(*i)) << "\"];"
 	      << std::endl;
 	}
       out << "}" << std::endl;
     }
 
     inline transducer_dot::transducer_dot(const std::string& name)
-      : name_(name)
-    {}
+    {
+      std::ostringstream os;
+      std::set<char> to_escape;
+      to_escape.insert('"');
+      (utility::setesc(to_escape))(os) << utility::escaper<std::string>(name);
+      name_ = std::string("\"") + os.str();
+    }
 
     template<typename Saver, typename Conv>
     void transducer_dot::operator()(std::ostream& out, const Saver& s,
@@ -112,12 +122,11 @@ namespace vcsn
       typedef typename Saver::automaton_t auto_t;
       AUTOMATON_TYPES(auto_t);
       const auto_t& a = s.automaton();
-      std::string   name("automaton_name");
       unsigned count = 0;
       std::map<hstate_t, unsigned> state_map;
 
       out << "digraph vcsn {" << std::endl
-	  << "label=\"" << name_ << ' ' << a << "\";" << std::endl
+	  << "label=\"" << name_.c_str() + 1 << ' ' << a << "\";" << std::endl
 	  << "node [shape=circle];" << std::endl;
 
       for (typename auto_t::state_iterator i = a.states().begin();
@@ -127,10 +136,10 @@ namespace vcsn
 	  unsigned c = state_map[*i] = count++;
 	  if (a.is_initial(*i))
 	    {
-	      out << name << count
-		  << " [style=invis,label=\"\",width=.01,height=.01];"
+	      out << name_ << count
+		  << "\" [style=invis,label=\"\",width=.01,height=.01];"
 		  << std::endl
-		  << name << count << " -> " << name << c ;
+		  << name_ << count << "\" -> " << name_ << c << '"';
 	      std::ostringstream o;
 	      series_set_elt_t ss = a.get_initial(*i);
 	      for_each_const_(series_set_elt_t::support_t, s, ss.supp())
@@ -143,10 +152,10 @@ namespace vcsn
 	    }
 	  if (a.is_final(*i))
 	    {
-	      out << name << count
-		  << " [style=invis,label=\"\",width=.01,height=.01];"
+	      out << name_ << count
+		  << "\" [style=invis,label=\"\",width=.01,height=.01];"
 		  << std::endl
-		  << name << c << " -> "  << name << count;
+		  << name_ << c << "\" -> " << name_ << count << '"';
 	      std::ostringstream o;
 	      series_set_elt_t ss = a.get_final(*i);
 	      for_each_const_(series_set_elt_t::support_t, s, ss.supp())
@@ -157,15 +166,15 @@ namespace vcsn
 		  << std::endl;
 	      ++count;
 	    }
-	  out << name << c << " [label=\"" << *i << "\"];" << std::endl;
+	  out << name_ << c << "\" [label=\"" << *i << "\"];" << std::endl;
 	}
       for (typename auto_t::edge_iterator i = a.edges().begin();
 	   i != a.edges().end();
 	   ++i)
 	{
-	  out << name << state_map[a.origin_of(*i)]
-	      << " -> "
-	      << name << state_map[a.aim_of(*i)];
+	  out << name_ << state_map[a.origin_of(*i)]
+	      << "\" -> "
+	      << name_ << state_map[a.aim_of(*i)] << '"';
 	  std::ostringstream o;
 	  series_set_elt_t ss = a.series_of(*i);
 	  for_each_const_(series_set_elt_t::support_t, s, ss.supp())
