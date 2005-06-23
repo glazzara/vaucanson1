a8f6391f5e948873d386d1dcede701f7d9956871
Sarah O'Connor <sarah.o-connor@lrde.epita.fr>
2005-06-23 20:17:46 +0000

    2005-06-23  Sarah O'Connor  <sarah.o-connor@lrde.epita.fr>
    	* src/demos/function_library/algorithms.cc,
    	* src/demos/function_library/rt_tdc_algorithms.cc,
    	* src/demos/function_library/tdc_algorithms.cc,
    	* src/demos/function_library/Makefile.am: New. Generate programs which
    	offer the possibility to use many algorithms of Vaucanson on several
    	automata types.
    
    	* src/demos/Makefile.am,
    	* configure.ac: Update

---
+++ src/demos/function_library/algorithms.cc
@@ -0,0 +1,473 @@
+// algorithms.cc: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2004, 2005 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//    * Sarah O'Connor <sarah.o-connor@lrde.epita.fr>
+//    * Louis-Noel Pouchet <louis-noel.pouchet@lrde.epita.fr>
+//
+
+/**
+ * @file algorithms.cc
+ *
+ * Run  various algorithms.  Results are  dumped  in a  XML format  on
+ * stdout.
+ *
+ * Valid invocations are:
+ *
+ * closure <file>
+ * realtime <file>
+ * transpose <file>
+ * product <file1> <file2>
+ * power <file> <n>
+ * quotient <file>
+ * accessible <file>
+ * coaccessible <file>
+ * trim <file>
+ * concatenate <file1> <file2>
+ * sum <file1> <file2>
+ * standardize <file>
+ * normalize <file>
+ * eval <file> <string>
+ * aut_to_exp <file> [state ordering for for eliminations]
+ * expand <exp>
+ * standard_of <exp>
+ * thompson_of <exp>
+ * derived_terms <exp>
+ * determinize <file>
+ * minimize <file> [-m for Moore minimization, -h for Hopcroft minimization]
+ * display <file>
+ * info <file>
+ *
+ */
+
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <list>
+
+#include CONTEXT_HEADER
+#include <vaucanson/xml/XML.hh>
+
+#include <vaucanson/algorithms/derived_terms_automaton.hh>
+#include <vaucanson/algorithms/product.hh>
+#include <vaucanson/algorithms/transpose.hh>
+#include <vaucanson/algorithms/eval.hh>
+#include <vaucanson/algorithms/determinize.hh>
+#include <vaucanson/algorithms/closure.hh>
+#include <vaucanson/algorithms/minimization_hopcroft.hh>
+#include <vaucanson/algorithms/minimization_moore.hh>
+#include <vaucanson/algorithms/trim.hh>
+#include <vaucanson/algorithms/concatenate.hh>
+#include <vaucanson/algorithms/sum.hh>
+#include <vaucanson/algorithms/krat_exp_expand.hh>
+#include <vaucanson/algorithms/accessible.hh>
+#include <vaucanson/algorithms/realtime.hh>
+#include <vaucanson/algorithms/standard.hh>
+#include <vaucanson/algorithms/normalized.hh>
+#include <vaucanson/tools/dot_display.hh>
+
+using namespace CONTEXT_NAMESPACE;
+
+using namespace vcsn;
+using namespace vcsn::io;
+using vcsn::xml::XML;
+
+static
+void
+usage(int, char** argv)
+{
+  std::cerr << "Usage: " << std::endl
+	    << "\t" << argv[0] << " <algorithm> <args...>" << std::endl;
+  exit(1);
+}
+
+static
+alphabet_t
+alphabet()
+{
+  alphabet_t	a;
+  a.insert('a');
+  a.insert('b');
+  return a;
+}
+
+static
+rat_exp_t
+get_exp(std::string s)
+{
+  return new_rat_exp(alphabet(), s);
+}
+
+static
+automaton_t
+get_aut(std::string s)
+{
+  std::istream* is (s == "-" ? &std::cin : new std::ifstream (s.c_str()));
+  if (not is->fail())
+    {
+      using namespace vcsn::io;
+      using namespace vcsn::xml;
+
+      automaton_t a = new_automaton(alphabet());
+      *is >> automaton_loader(a, string_out (), XML ());
+
+      if (s != "-")
+	delete is;
+      return a;
+    }
+  else
+    {
+      std::cerr << "FATAL: Could not load automaton." << std::endl;
+      exit(1);
+    }
+}
+
+static
+void
+derived_terms_automaton_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  rat_exp_t	e = get_exp(argv[2]);
+  automaton_t	a = new_automaton(alphabet());
+  derived_terms_automaton(a, e);
+  std::cout << automaton_saver(a, string_out (), XML ());
+}
+
+static
+void
+aut_to_exp_command(int argc, char** argv)
+{
+  if (argc < 3)
+    usage(argc, argv);
+  else if (argc == 3)
+    std::cout << aut_to_exp(get_aut(argv[2]), DMChooser()) << std::endl;
+  else
+    {
+      automaton_t		a = get_aut(argv[2]);
+      if (a.states().size() != unsigned (argc - 3))
+	{
+	  std::cerr << "ERROR: Invalid state list." << std::endl;
+	  exit(2);
+	}
+
+      std::list<hstate_t>	l;
+      for (int i = 3; i < argc; ++i)
+	{
+	  std::istringstream	is (argv[i]);
+	  int			s;
+
+	  is >> s;
+	  if (s < 0 or unsigned (s) >= a.states().size())
+	    {
+	      std::cerr << "ERROR: " << s << " is not a valid state."
+			<< std::endl;
+	      exit(2);
+	    }
+	  l.push_back(s);
+	}
+
+      std::cout << aut_to_exp(a, ListChooser (l)) << std::endl;
+    }
+}
+
+static
+void
+product_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  std::cout << automaton_saver(product(get_aut(argv[2]), get_aut(argv[3])),
+			       string_out (),
+			       XML ());
+}
+
+void
+eval_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  std::cout << eval(realtime(get_aut(argv[2])), std::string (argv[3]));
+}
+
+void
+power_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  int n = atoi(argv[3]);
+
+  automaton_t a = get_aut(argv[2]);
+  automaton_t p(a);
+
+  for (int i = 1; i <= n; ++i)
+    p = product(p, a);
+
+  std::cout << automaton_saver(p, string_out (), XML ());
+}
+
+static
+void
+display_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  vcsn::tools::dot_display(get_aut(argv[2]), "A", true);
+}
+
+static
+void
+info_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  automaton_t a = get_aut(argv[2]);
+
+  std::cout << "States: " << a.states().size() << std::endl;
+  std::cout << "Transitions: " << a.edges().size() << std::endl;
+  std::cout << "Initial states: " << a.initial().size() << std::endl;
+  std::cout << "Final states: " << a.final().size() << std::endl;
+}
+
+static
+void
+expand_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  std::cout << expand(get_exp(argv[2])) << std::endl;
+}
+
+static
+void
+minimize_command(int argc, char** argv)
+{
+  if (argc < 3)
+    usage(argc, argv);
+  else if (argc == 3)
+    std::cout <<  automaton_saver(minimization_hopcroft(get_aut(argv[2])),
+				  string_out (), XML ())
+	      << std::endl;
+  else
+    {
+      std::string	arg (argv[3]);
+      automaton_t	a = get_aut(argv[2]);
+
+      if (arg == "-m")
+	std::cout <<  automaton_saver(minimization_moore(get_aut(argv[2])),
+				      string_out (), XML ())
+		  << std::endl;
+
+      else if (arg == "-h")
+	std::cout <<  automaton_saver(minimization_hopcroft(get_aut(argv[2])),
+				      string_out (), XML ())
+		  << std::endl;
+    }
+}
+
+static
+void
+concatenate_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  std::cout << automaton_saver(concatenate(get_aut(argv[2]), get_aut(argv[3])),
+			       string_out (), XML ())
+	    << std::endl;
+}
+
+static
+void
+sum_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  std::cout << automaton_saver(sum(get_aut(argv[2]), get_aut(argv[3])),
+			       string_out (), XML ())
+	    << std::endl;
+}
+
+static
+void
+closure_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+   std::cout << automaton_saver(accessible(closure(get_aut(argv[2]))),
+				string_out (), XML ())
+	     << std::endl;
+}
+
+static
+void
+determinize_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+   std::cout << automaton_saver(determinize(realtime(get_aut(argv[2]))),
+				string_out (), XML ())
+	     << std::endl;
+}
+
+static
+void
+quotient_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+   std::cout << automaton_saver(quotient(realtime(get_aut(argv[2]))),
+				string_out (), XML ())
+	     << std::endl;
+}
+
+static
+void
+standardize_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  automaton_t a = get_aut(argv[2]);
+  standardize(a);
+
+  std::cout << automaton_saver(a, string_out (), XML ()) << std::endl;
+}
+
+#define ONE_ARG_COMMAND(GetArg, Algo) one_arg_command_ ## Algo ## _ ## GetArg
+
+#define DEFINE_ONE_ARG_COMMAND(GetArg, Algo)		\
+static							\
+void							\
+ONE_ARG_COMMAND(GetArg, Algo)(int argc, char** argv)	\
+{							\
+  if (argc != 3)					\
+    usage(argc, argv);					\
+  std::cout << automaton_saver(Algo(GetArg(argv[2])),	\
+			       string_out (),		\
+			       XML ());			\
+}
+
+DEFINE_ONE_ARG_COMMAND(get_exp, standard_of)
+DEFINE_ONE_ARG_COMMAND(get_exp, thompson_of)
+DEFINE_ONE_ARG_COMMAND(get_aut, trim)
+DEFINE_ONE_ARG_COMMAND(get_aut, transpose)
+DEFINE_ONE_ARG_COMMAND(get_aut, realtime)
+DEFINE_ONE_ARG_COMMAND(get_aut, coaccessible)
+DEFINE_ONE_ARG_COMMAND(get_aut, accessible)
+DEFINE_ONE_ARG_COMMAND(get_aut, normalize)
+
+#undef DEFINE_ONE_ARG_COMMAND
+
+const struct
+{
+  char*	name;
+  void	(*command)(int, char**);
+}
+command_map[] =
+  {
+    { "expand",			expand_command				},
+    { "standard_of",		ONE_ARG_COMMAND(get_exp, standard_of)	},
+    { "thompson_of",		ONE_ARG_COMMAND(get_exp, thompson_of)	},
+    { "derived_terms",		derived_terms_automaton_command		},
+    { "aut_to_exp",		aut_to_exp_command			},
+    { "quotient",		quotient_command			},
+    { "product",		product_command				},
+    { "closure",		closure_command				},
+    //    { "is_empty",		ONE_ARG_COMMAND(get_aut, is_void)	},
+    { "determinize",		determinize_command			},
+    { "minimize",		minimize_command			},
+    { "trim",			ONE_ARG_COMMAND(get_aut, trim)		},
+    { "transpose",		ONE_ARG_COMMAND(get_aut, transpose)	},
+    { "accessible",		ONE_ARG_COMMAND(get_aut, coaccessible)	},
+    { "coaccessible",		ONE_ARG_COMMAND(get_aut, accessible)	},
+    { "sum",			sum_command				},
+    { "concatenate",		concatenate_command			},
+    { "realtime",		ONE_ARG_COMMAND(get_aut, realtime)	},
+    { "eval",			eval_command				},
+    { "power",			power_command				},
+    { "normalize",		ONE_ARG_COMMAND(get_aut, normalize)	},
+    { "standardize",		standardize_command			},
+    { "display",		display_command				},
+    { "info",			info_command				},
+    { 0,			0					}
+  };
+
+#undef ONE_ARG_COMMAND
+
+int
+main(int argc, char** argv)
+{
+  if (argc < 2)
+    usage(argc, argv);
+
+  std::string cmd (argv[1]);
+  int i;
+
+  for (i = 0; command_map[i].name != 0; ++i)
+    if (cmd == command_map[i].name)
+      {
+	command_map[i].command(argc, argv);
+	break ;
+      }
+  if (command_map[i].name == 0)
+    {
+      std::cerr << "Available algorithms:" << std::endl;
+      std::cerr << " * standard_of"  << std::endl;
+      std::cerr << " * expand"  << std::endl;
+      std::cerr << " * thompson_of"  << std::endl;
+      std::cerr << " * derived_terms"  << std::endl;
+      std::cerr << " * aut_to_exp"  << std::endl;
+      std::cerr << " * quotient"  << std::endl;
+      std::cerr << " * product"  << std::endl;
+      std::cerr << " * determinize"  << std::endl;
+      std::cerr << " * minimize"  << std::endl;
+      std::cerr << " * closure"  << std::endl;
+      std::cerr << " * is_empty"  << std::endl;
+      std::cerr << " * accessible"  << std::endl;
+      std::cerr << " * coaccessible"  << std::endl;
+      std::cerr << " * realtime"  << std::endl;
+      std::cerr << " * power"  << std::endl;
+      std::cerr << " * trim"  << std::endl;
+      std::cerr << " * eval"  << std::endl;
+      std::cerr << " * transpose"  << std::endl;
+      std::cerr << " * display"  << std::endl;
+      std::cerr << " * info"  << std::endl;
+      exit(1);
+    }
+}
---
+++ src/demos/function_library/tdc_algorithms.cc
@@ -0,0 +1,435 @@
+// tdc_algorithms.cc: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+// Copyright (C) 2005 The Vaucanson Group.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// The Vaucanson Group represents the following contributors:
+//    * Jacques Sakarovitch <sakarovitch@enst.fr>
+//    * Sylvain Lombardy <lombardy@iafa.jussieu.fr>
+//    * Thomas Claveirole <thomas.claveirole@lrde.epita.fr>
+//    * Loic Fosse <loic.fosse@lrde.epita.fr>
+//    * Thanh-Hoc Nguyen <nguyen@enst.fr>
+//    * Raphael Poss <raphael.poss@lrde.epita.fr>
+//    * Yann Regis-Gianas <yann.regis-gianas@lrde.epita.fr>
+//    * Maxime Rey <maxime.rey@lrde.epita.fr>
+//    * Sarah O'Connor <sarah.o-connor@lrde.epita.fr>
+//    * Louis-Noel Pouchet <louis-noel.pouchet@lrde.epita.fr>
+//
+
+/**
+ * @file tdc_algorithms.cc
+ *
+ * Run various algorithms on realtime transducers.  Results are dumped
+ * in a XML format on stdout.
+ *
+ * Valid invocations are:
+ *
+ * closure <file>
+ * sub-normalize <file>
+ * is-sub-normalized <file>
+ * compose <file> <file>
+ * evaluation <file> <file>
+ * evaluation_aut <file> <file>
+ * domain <file>
+ * image <file>
+ * composition-cover <file>
+ * composition-co-cover <file>
+ * b-compose <file> <file>
+ * to-rt-tdc <file>
+ * intersection <file>
+ * display
+ * trim
+ * transpose
+ * info
+ *
+ */
+
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <list>
+
+#include CONTEXT_HEADER
+#include <vaucanson/xml/XML.hh>
+
+#include <vaucanson/algorithms/closure.hh>
+#include <vaucanson/algorithms/normalized_composition.hh>
+#include <vaucanson/algorithms/sub_normalize.hh>
+#include <vaucanson/algorithms/evaluation_fmp.hh>
+#include <vaucanson/algorithms/projections_fmp.hh>
+#include <vaucanson/algorithms/outsplitting.hh>
+#include <vaucanson/algorithms/transpose.hh>
+#include <vaucanson/algorithms/fmp_to_realtime.hh>
+#include <vaucanson/tools/dot_display.hh>
+#include <vaucanson/boolean_automaton.hh>
+#include <vaucanson/boolean_transducer.hh>
+
+using namespace CONTEXT_NAMESPACE;
+
+using namespace vcsn;
+using namespace vcsn::io;
+using vcsn::xml::XML;
+
+static
+void
+usage(int, char** argv)
+{
+  std::cerr << "Usage: " << std::endl
+	    << "\t" << argv[0] << " <algorithm> <args...>" << std::endl;
+  exit(1);
+}
+
+static
+first_alphabet_t
+first_alphabet()
+{
+  first_alphabet_t	a;
+  a.insert('a');
+  a.insert('b');
+  return a;
+}
+
+static
+second_alphabet_t
+second_alphabet()
+{
+  second_alphabet_t	a;
+  a.insert('a');
+  a.insert('b');
+  return a;
+}
+
+
+static
+automaton_t
+get_aut(std::string s)
+{
+  std::istream* is (s == "-" ? &std::cin : new std::ifstream (s.c_str()));
+  if (not is->fail())
+    {
+      using namespace vcsn::io;
+      using namespace vcsn::xml;
+
+      automaton_t a = new_automaton(first_alphabet(), second_alphabet());
+      *is >> automaton_loader(a, string_out (), XML ());
+
+      if (s != "-")
+	delete is;
+      return a;
+    }
+  else
+    {
+      std::cerr << "FATAL: Could not load automaton." << std::endl;
+      exit(1);
+    }
+}
+
+static
+boolean_automaton::automaton_t
+get_boolean_aut(std::string s)
+{
+  std::istream* is (s == "-" ? &std::cin : new std::ifstream (s.c_str()));
+  if (not is->fail())
+    {
+      using namespace vcsn::io;
+      using namespace vcsn::xml;
+
+      boolean_automaton::automaton_t a =
+	boolean_automaton::new_automaton(first_alphabet());
+      *is >> automaton_loader(a, string_out (), XML ());
+
+      if (s != "-")
+	delete is;
+      return a;
+    }
+  else
+    {
+      std::cerr << "FATAL: Could not load automaton." << std::endl;
+      exit(1);
+    }
+}
+
+
+static
+void
+compose_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  automaton_t a = get_aut(argv[2]);
+  automaton_t b = get_aut(argv[3]);
+
+  if (!is_sub_normalized(a))
+    a = sub_normalize(a);
+
+  if (!is_sub_normalized(b))
+    b = sub_normalize(b);
+
+  std::cout << automaton_saver(normalized_composition(a,b),
+			       string_out (),
+			       XML ());
+}
+
+
+static
+void
+evaluation_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  std::cout << evaluation(get_aut(argv[2]),
+			  boolean_automaton::new_rat_exp(first_alphabet(),
+							 argv[3]))
+	    << std::endl;
+}
+
+static
+void
+evaluation_aut_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  boolean_automaton::automaton_t a = get_boolean_aut(argv[3]);
+  boolean_automaton::automaton_t res =
+    boolean_automaton::new_automaton(second_alphabet());
+
+  evaluation_fmp(get_aut(argv[2]), a, res);
+
+  std::cout << automaton_saver(res, string_out (), XML ());
+}
+
+static
+void
+b_compose_command(int argc, char** argv)
+{
+  if (argc != 4)
+    usage(argc, argv);
+
+  automaton_t a = get_aut(argv[2]);
+  automaton_t b = get_aut(argv[3]);
+
+  if (!is_sub_normalized(a))
+    a = sub_normalize(a);
+
+  if (!is_sub_normalized(b))
+    b = sub_normalize(b);
+
+  std::cout << automaton_saver(b_composition(a,b),
+			       string_out (),
+			       XML ());
+}
+
+static
+void
+is_subnormalized_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  std::cout << is_sub_normalized(get_aut(argv[2])) << std::endl;
+}
+
+static
+void
+domain_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  boolean_automaton::automaton_t a =
+    boolean_automaton::new_automaton(first_alphabet());
+
+  domain(get_aut(argv[2]), a);
+
+  std::cout << automaton_saver(a, string_out (), XML ());
+}
+
+static
+void
+image_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  boolean_automaton::automaton_t a =
+    boolean_automaton::new_automaton(second_alphabet());
+
+  image(get_aut(argv[2]), a);
+
+  std::cout << automaton_saver(a, string_out (), XML ());
+}
+
+
+static
+void
+identity_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  boolean_automaton::automaton_t a = get_boolean_aut(argv[2]);
+
+  automaton_t fmp = new_automaton(first_alphabet(), second_alphabet());
+
+  identity(a, fmp);
+
+  std::cout << automaton_saver(fmp, string_out (), XML ());
+}
+
+
+static
+void
+fmp_to_realtime_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  boolean_transducer::automaton_t a =
+    boolean_transducer::new_automaton(first_alphabet(), second_alphabet());
+
+  fmp_to_realtime(get_aut(argv[2]), a);
+  std::cout << automaton_saver(a, string_out (), XML ());
+}
+
+static
+void
+display_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  vcsn::tools::dot_display(get_aut(argv[2]), "A", true);
+}
+
+static
+void
+info_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+  automaton_t a = get_aut(argv[2]);
+
+  std::cout << "States: " << a.states().size() << std::endl;
+  std::cout << "Transitions: " << a.edges().size() << std::endl;
+  std::cout << "Initial states: " << a.initial().size() << std::endl;
+  std::cout << "Final states: " << a.final().size() << std::endl;
+}
+
+static
+void
+closure_command(int argc, char** argv)
+{
+  if (argc != 3)
+    usage(argc, argv);
+
+   std::cout << automaton_saver(accessible(closure(get_aut(argv[2]))),
+				string_out (), XML ())
+	     << std::endl;
+}
+
+#define ONE_ARG_COMMAND(GetArg, Algo) one_arg_command_ ## Algo ## _ ## GetArg
+
+#define DEFINE_ONE_ARG_COMMAND(GetArg, Algo)		\
+static							\
+void							\
+ONE_ARG_COMMAND(GetArg, Algo)(int argc, char** argv)	\
+{							\
+  if (argc != 3)					\
+    usage(argc, argv);					\
+  std::cout << automaton_saver(Algo(GetArg(argv[2])),	\
+			       string_out (),		\
+			       XML ());			\
+}
+
+DEFINE_ONE_ARG_COMMAND(get_aut, sub_normalize)
+DEFINE_ONE_ARG_COMMAND(get_aut, outsplitting)
+DEFINE_ONE_ARG_COMMAND(get_aut, insplitting)
+DEFINE_ONE_ARG_COMMAND(get_aut, trim)
+DEFINE_ONE_ARG_COMMAND(get_aut, transpose)
+#undef DEFINE_ONE_ARG_COMMAND
+
+const struct
+{
+  char*	name;
+  void	(*command)(int, char**);
+}
+command_map[] =
+  {
+    { "sub-normalize",		ONE_ARG_COMMAND(get_aut, sub_normalize)	},
+    { "is-sub-normalized",	is_subnormalized_command		},
+    { "compose",		compose_command				},
+    { "evaluation",		evaluation_command			},
+    { "evaluation_aut",		evaluation_aut_command			},
+    { "domain",			domain_command				},
+    { "image",			image_command				},
+    { "intersection",		identity_command			},
+    { "composition-cover",	ONE_ARG_COMMAND(get_aut, outsplitting)	},
+    { "composition-co-cover",	ONE_ARG_COMMAND(get_aut, insplitting)  	},
+    { "to-rt-tdc",		fmp_to_realtime_command			},
+    { "closure",		closure_command				},
+    { "trim",			ONE_ARG_COMMAND(get_aut, trim)		},
+    { "b-compose",		b_compose_command			},
+    { "transpose",		ONE_ARG_COMMAND(get_aut, transpose)	},
+    { "display",		display_command				},
+    { "info",			info_command				},
+    { 0,			0					}
+  };
+
+#undef ONE_ARG_COMMAND
+
+int
+main(int argc, char** argv)
+{
+  if (argc < 2)
+    usage(argc, argv);
+
+  std::string cmd (argv[1]);
+  int i;
+
+  for (i = 0; command_map[i].name != 0; ++i)
+    if (cmd == command_map[i].name)
+      {
+	command_map[i].command(argc, argv);
+	break ;
+      }
+  if (command_map[i].name == 0)
+    {
+      std::cerr << "Available algorithms:" << std::endl;
+      std::cerr << " * sub-normalize"  << std::endl;
+      std::cerr << " * is-sub-normalized"  << std::endl;
+      std::cerr << " * compose"  << std::endl;
+      std::cerr << " * b-compose"  << std::endl;
+      std::cerr << " * composition-cover"  << std::endl;
+      std::cerr << " * composition-co-cover"  << std::endl;
+      std::cerr << " * evaluation"  << std::endl;
+      std::cerr << " * evaluation_aut"  << std::endl;
+      std::cerr << " * domain"  << std::endl;
+      std::cerr << " * image"  << std::endl;
+      std::cerr << " * intersection"  << std::endl;
+      std::cerr << " * to-rt-tdc"  << std::endl;
+      std::cerr << " * closure"  << std::endl;
+      std::cerr << " * display"  << std::endl;
+      std::cerr << " * trim" << std::endl;
+      std::cerr << " * transpose" << std::endl;
+      std::cerr << " * info" << std::endl;
+      exit(1);
+    }
+}
