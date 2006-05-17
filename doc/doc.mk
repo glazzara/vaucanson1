2261a628687ffa799de02ed06c685a70befd54a5
Akim Demaille <akim@lrde.epita.fr>
2006-05-17 08:02:11 +0000

    2006-05-17  Akim Demaille  <akim@epita.fr>
    	Help distcheck.
    	* doc/manual/Makefile.am (SRC_FILES): Add the missing bits.
    	* doc/doc.mk: Fix the order of the rules.

--- doc/doc.mk
+++ doc/doc.mk
@@ -1,16 +1,20 @@
 # -*- Makefile -*-
 
+# Issue these rules before including tex.mk so that txt -> html is
+# preferred over txt -> tex -> html.  The latter is longer, but
+# because we actually also provide the tex output, so in fact the path
+# has the same length: tex -> html.
+%.html: %.txt
+	$(RST2HTML) --input-encoding=iso-8859-1 $< >$@
+
+%.tex: %.txt
+	$(RST2TEX) --input-encoding=iso-8859-1 $< >$@
+
 share_dir = $(top_srcdir)/doc/share
 # Cannot factor here because Automake needs to be able to read
 # the full path and does not accept variables.
 include $(top_srcdir)/doc/share/make/share.mk
 include $(top_srcdir)/doc/share/make/tex.mk
-
-%.tex: %.txt
-	$(RST2TEX) --input-encoding=iso-8859-1 $< >$@
-
-%.html: %.txt
-	$(RST2HTML) --input-encoding=iso-8859-1 $< >$@
 
 docdir = $(datadir)/doc/$(PACKAGE_TARNAME)
 CLEANFILES = *.dvi *.aux *blg *.out *.toc *.lot *.tmp *.log
