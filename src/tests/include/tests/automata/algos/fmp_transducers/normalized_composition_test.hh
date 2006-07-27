ca56d9545169fdb3ffd65e2709b0e44d65b77619
Akim Demaille <akim@lrde.epita.fr>
2006-07-27 10:26:17 +0000

    Spare Autotool cycles.
    	* src/tests/include/tests/context_headers/fmp_transducers/Makefile.am: Remove.
    	* src/tests/include/tests/context_headers/Makefile.am: Remove.
    	* src/tests/include/tests/context_headers/automata/Makefile.am: Remove.
    	* src/tests/include/tests/context_headers/transducers/Makefile.am: Remove.
    	* src/tests/include/tests/design_pattern/Makefile.am: Remove.
    	* src/tests/include/tests/Makefile.am: Remove.
    	* src/tests/include/tests/automata/implementation_check/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/fmp_transducers/normalized_composition_test.hh: .
    	* src/tests/include/tests/automata/algos/fmp_transducers/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/series_multiplicity_transducers/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/label_aware_graphs/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/freemonoid_labeled_graphs/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/boolean_rw_transducers/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/graphs/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/boolean_automata/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/labeled_graphs/Makefile.am: Remove.
    	* src/tests/include/tests/automata/algos/letter_combination_labeled_graphs/Makefile.am: Remove.
    	* src/tests/include/tests/automata/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/alphabets/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/free_monoid/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/free_monoid_product/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/boolean/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/tests/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/main/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/derivations/partial_derivation/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/derivations/derivation/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/derivations/cderivation/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/derivations/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/krat/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/misc/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/series/polynom/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/numerical_semiring/Makefile.am: Remove.
    	* src/tests/include/tests/algebra/semiring/Makefile.am: Remove.
    	* src/tests/include/tests/check/Makefile.am: Remove.
    	* src/tests/include/Makefile.am: Put all the data in here.
    
    	* src/tests/Makefile.am (SUBDIRS): Don't forget include.
    	* data/automata/tdc-rw/Makefile.am: New.
    	Should have been added earlier, sorry.
    	* configure.ac: Adjust.

--- src/tests/include/tests/automata/algos/fmp_transducers/normalized_composition_test.hh
+++ src/tests/include/tests/automata/algos/fmp_transducers/normalized_composition_test.hh
@@ -36,60 +36,60 @@ normalized_composition_test(tests::Tester& t)
   typedef typename first_monoid_t::alphabet_t	first_alphabet_t;
   typedef typename second_monoid_t::alphabet_t	second_alphabet_t;
 
-  first_alphabet_t		first_at;
+  first_alphabet_t first_at;
   first_at.insert('a');
   first_at.insert('b');
 
-  second_alphabet_t		second_at;
+  second_alphabet_t second_at;
   second_at.insert('x');
   second_at.insert('y');
 
-  second_alphabet_t		third_at;
+  second_alphabet_t third_at;
   third_at.insert('u');
   third_at.insert('v');
 
-  first_monoid_t		first_md (first_at);
-  second_monoid_t		second_md (second_at);
-  second_monoid_t		third_md (third_at);
+  first_monoid_t  first_md (first_at);
+  second_monoid_t second_md (second_at);
+  second_monoid_t third_md (third_at);
 
-  monoid_t			fmp(first_md, second_md);
-  monoid_t			fmp2(second_md, third_md);
-  monoid_t			fmp3(first_md, third_md);
+  monoid_t fmp(first_md, second_md);
+  monoid_t fmp2(second_md, third_md);
+  monoid_t fmp3(first_md, third_md);
 
-  semiring_t			sg;
+  semiring_t sg;
 
-  series_set_t			ss (sg, fmp);
-  series_set_t			ss2 (sg, fmp2);
-  series_set_t			ss3 (sg, fmp3);
+  series_set_t ss (sg, fmp);
+  series_set_t ss2 (sg, fmp2);
+  series_set_t ss3 (sg, fmp3);
 
-  automata_set_t		aa (ss);
-  automata_set_t		aa2 (ss2);
-  automata_set_t		aa3 (ss3);
+  automata_set_t aa (ss);
+  automata_set_t aa2 (ss2);
+  automata_set_t aa3 (ss3);
 
-  automaton_t		t1(aa);
-  automaton_t		t2(aa2);
-  automaton_t		t3(aa3);
+  automaton_t t1(aa);
+  automaton_t t2(aa2);
+  automaton_t t3(aa3);
 
-  series_set_elt_t	series_elt1(ss);
-  series_set_elt_t	series_elt2(ss);
-  series_set_elt_t	series_elt3(ss);
-  series_set_elt_t	series_elt4(ss);
+  series_set_elt_t series_elt1(ss);
+  series_set_elt_t series_elt2(ss);
+  series_set_elt_t series_elt3(ss);
+  series_set_elt_t series_elt4(ss);
 
 
-  series_set_elt_t	series_elt21(ss);
-  series_set_elt_t	series_elt22(ss);
-  series_set_elt_t	series_elt23(ss);
-  series_set_elt_t	series_elt24(ss);
+  series_set_elt_t series_elt21(ss);
+  series_set_elt_t series_elt22(ss);
+  series_set_elt_t series_elt23(ss);
+  series_set_elt_t series_elt24(ss);
 
-  monoid_elt_value_t	fmp_elt1 ("a", "");
-  monoid_elt_value_t	fmp_elt2 ("b", "");
-  monoid_elt_value_t	fmp_elt3 ("", "y");
-  monoid_elt_value_t	fmp_elt4 ("", "x");
+  monoid_elt_value_t fmp_elt1 ("a", "");
+  monoid_elt_value_t fmp_elt2 ("b", "");
+  monoid_elt_value_t fmp_elt3 ("", "y");
+  monoid_elt_value_t fmp_elt4 ("", "x");
 
-  monoid_elt_value_t	fmp_elt21 ("", "u");
-  monoid_elt_value_t	fmp_elt22 ("x", "");
-  monoid_elt_value_t	fmp_elt23 ("y", "");
-  monoid_elt_value_t	fmp_elt24 ("", "v");
+  monoid_elt_value_t fmp_elt21 ("", "u");
+  monoid_elt_value_t fmp_elt22 ("x", "");
+  monoid_elt_value_t fmp_elt23 ("y", "");
+  monoid_elt_value_t fmp_elt24 ("", "v");
 
   semiring_elt_value_t	semi_elt =
     algebra::identity_as<semiring_elt_value_t>::of(sg).value();
@@ -104,13 +104,13 @@ normalized_composition_test(tests::Tester& t)
   series_elt23.assoc(fmp_elt23, semi_elt);
   series_elt24.assoc(fmp_elt24, semi_elt);
 
-  hstate_t	st1 = t1.add_state();
-  hstate_t	st2 = t1.add_state();
-  hstate_t	st3 = t1.add_state();
+  hstate_t st1 = t1.add_state();
+  hstate_t st2 = t1.add_state();
+  hstate_t st3 = t1.add_state();
 
-  hstate_t	st21 = t2.add_state();
-  hstate_t	st22 = t2.add_state();
-  hstate_t	st23 = t2.add_state();
+  hstate_t st21 = t2.add_state();
+  hstate_t st22 = t2.add_state();
+  hstate_t st23 = t2.add_state();
 
 
   t1.set_initial(st1);
@@ -121,20 +121,15 @@ normalized_composition_test(tests::Tester& t)
   t2.set_initial(st23);
   t2.set_final(st21);
 
-  htransition_t			h1 = t1.add_series_transition(st1, st2, series_elt1);
-  htransition_t			h2 = t1.add_series_transition(st2, st1, series_elt3);
-  htransition_t			h3 = t1.add_series_transition(st1, st3, series_elt4);
-  htransition_t			h4 = t1.add_series_transition(st3, st1, series_elt2);
+  htransition_t h1 = t1.add_series_transition(st1, st2, series_elt1);
+  htransition_t h2 = t1.add_series_transition(st2, st1, series_elt3);
+  htransition_t h3 = t1.add_series_transition(st1, st3, series_elt4);
+  htransition_t h4 = t1.add_series_transition(st3, st1, series_elt2);
 
-
-  htransition_t			h21 = t2.add_series_transition(st21, st22,
-							       series_elt23);
-  htransition_t			h22 = t2.add_series_transition(st22, st21,
-							       series_elt21);
-  htransition_t			h23 = t2.add_series_transition(st21, st23,
-							       series_elt24);
-  htransition_t			h24 = t2.add_series_transition(st23, st21,
-							       series_elt22);
+  htransition_t h21 = t2.add_series_transition(st21, st22, series_elt23);
+  htransition_t h22 = t2.add_series_transition(st22, st21, series_elt21);
+  htransition_t h23 = t2.add_series_transition(st21, st23, series_elt24);
+  htransition_t h24 = t2.add_series_transition(st23, st21, series_elt22);
 
   t3 = compose(t1, t2);
 
