ba3326eb5b20faeafd059ac180e284e1909037ee
Akim Demaille <akim@lrde.epita.fr>
2006-07-31 08:11:56 +0000

    2006-07-31  Akim Demaille  <akim@lrde.epita.fr>
    	Fix sanity tests.
    	* src/tests/sanity/Makefile.am (TESTS, TESTS_LOGS): Define them.
    	Do it before including build-aux/check.mk, otherwise its default
    	values apply.
    	* src/tests/sanity/generate_files.sh: Better error messages.
    	Don't define

--- src/tests/sanity/generate_files.sh
+++ src/tests/sanity/generate_files.sh
@@ -2,13 +2,20 @@
 
 set -e
 
-test "x$1" != "x"
+if test "x$1" = "x"; then
+  echo >&2 "usage: $0 SRCDIR"
+  exit 1
+fi
 cd "$1"
 
 top_srcdir=../../..
 build_aux_dir=$top_srcdir/build-aux
 move_if_change=$build_aux_dir/move-if-change
-test -f $move_if_change
+
+if test ! -f $move_if_change; then
+  echo >&2 "$0: no such file: $move_if_change"
+  exit 1
+fi
 
 # The file to generate.
 out=check-programs.mk
@@ -44,8 +51,6 @@ check_PROGRAMS += \$(ProgsXml)
 EOF
 
 echo >> $tmp
-echo "TESTS = \$(check_PROGRAMS)" >> $tmp
-
 # Set test sources and CPPFLAGS.
 awk '
   {
