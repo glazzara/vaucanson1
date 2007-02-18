b921f4daac28cfc66a2219cf1ab50990f8decdb6
Guillaume Leroi <guillaume.leroi@lrde.epita.fr>
2007-02-18 17:22:45 +0000

    Fix typo in doxygen documentation of standard.hh. 	* include/vaucanson/algorithms/standard.hh: Change boolean to Boolean.

--- include/vaucanson/algorithms/standard.hh
+++ include/vaucanson/algorithms/standard.hh
@@ -26,7 +26,7 @@
  * This  file contains different  operations which  can be  applied on
  * standard automata.
  *
- * For boolean automata, see ETA Chapter 1, Section 5.1.
+ * For Boolean automata, see ETA Chapter 1, Section 5.1.
  * For K-automata, see ETA Chapter 3, Section 2.
  * See also the correction of the exercice 2.24 p.540.
  *
