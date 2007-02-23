57de1aa80912f3b9aa9917b84b9a252a2088a743
Jimmy Ma <jimmy.ma@lrde.epita.fr>
2007-02-23 11:48:30 +0000

    Add support for libvcsn-*.la 	generate_libvcsn.rb script has been updated to handle 	all the types of automaton supported by vcsn. 	The use of static library has been replaced with shared library. 	* configure.ac: Add the subdirectories of lib/ 	* lib/Makefile.am: Update. 	* lib/generate_libvcsn.rb: Add support for libvcsn-*.la. 	* lib/vcsn-b/vcsn_b.hh: New. 	* lib/vcsn-fmp-tdc/vcsn_fmp_tdc.hh: New. 	* lib/vcsn-fmp-tdc: New. 	* lib/vcsn-r/vcsn_r.hh: New. 	* lib/vcsn-r: New. 	* lib/vcsn-tdc/vcsn_tdc.hh: New. 	* lib/vcsn-tdc: New. 	* lib/vcsn-z-fmp-tdc/vcsn_z_fmp_tdc.hh: New. 	* lib/vcsn-z-fmp-tdc: New. 	* lib/vcsn-z-max/vcsn_z_max.hh: New. 	* lib/vcsn-z-max: New. 	* lib/vcsn-z-min/vcsn_z_min.hh: New. 	* lib/vcsn-z-min: New. 	* lib/vcsn-z-tdc/vcsn_z_tdc.hh: New. 	* lib/vcsn-z-tdc: New. 	* lib/vcsn-z/vcsn_z.hh: New. 	* lib/vcsn-z: New.

--- lib/generate_libvcsn.rb
+++ lib/generate_libvcsn.rb
@@ -14,128 +14,220 @@
 #
 # The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
+# Array filled with the supported types of automaton
+vcsn = [ "vcsn-b", "vcsn-r", "vcsn-z", "vcsn-z-max", "vcsn-z-min",
+	 "vcsn-fmp-tdc", "vcsn-tdc", "vcsn-z-fmp-tdc", "vcsn-z-tdc" ]
+
+# testing if a source file needs to be created
+def create?(type, file)
+  (
+    ! File.exist?(type + "/" + File.basename(file, ".hh") + ".cc") or
+    File.new(file, "r").stat > File.new(type + "/" + File.basename(file, ".hh") + ".cc").stat
+  ) and
+  # FIXME list of ignored files for each shall be removed later
+  file !~ /krat_exp_linearize.hh/ and	# weird parsing which shall be done soon
+  (
+    (
+      ( "vcsn-b" == type or "vcsn-r" == type or "vcsn-z" == type or
+	"vcsn-z-max" == type or "vcsn-z-min" == type ) and
+      file !~ /backward_realtime.hh/ and	# realtime 
+      file !~ /realtime_decl.hh/ and		# realtime
+      file !~ /composition_cover.hh/ and	# transducer
+      file !~ /extension.hh/ and		# transducer
+      file !~ /invert.hh/ and			# transducer
+      file !~ /is_letterized.hh/ and		# transducer
+      file !~ /is_normalized.hh/ and		# transducer
+      file !~ /normalized_composition.hh/	# transducer
+    ) or
+    (
+      "vcsn-tdc" == type and
+      file !~ /accessible.hh/ and
+      file !~ /aut_to_exp.hh/ and
+      file !~ /backward_realtime.hh/ and	# should work ?
+      file !~ /complement.hh/ and
+      file !~ /complete.hh/ and
+      file !~ /composition_cover.hh/ and	# should work ?
+      file !~ /determinize.hh/ and
+      file !~ /extension.hh/ and
+      file !~ /invert.hh/ and			# should work ?
+      file !~ /is_letterized.hh/ and
+      file !~ /is_normalized.hh/ and
+      file !~ /minimization_hopcroft.hh/ and
+      file !~ /normalized_composition.hh/ and
+      file !~ /realtime_decl.hh/ and		# should work ?
+      file !~ /standard.hh/
+    ) or
+    (
+      "vcsn-fmp-tdc" == type and
+      file !~ /backward_realtime.hh/ and	# should work ?
+      file !~ /complement.hh/ and
+      file !~ /complete.hh/ and
+      file !~ /composition_cover.hh/ and	# should work ?
+      file !~ /determinize.hh/ and
+      file !~ /extension.hh/ and
+      file !~ /forward_realtime.hh/ and	 	# should work ?
+      file !~ /is_letterized.hh/ and
+      file !~ /is_normalized.hh/ and
+      file !~ /minimization_hopcroft.hh/ and
+      file !~ /minimization_moore.hh/ and
+      file !~ /normalized_composition.hh/ and
+      file !~ /realtime.hh/ and			# should work ?
+      file !~ /realtime_decl.hh/		# should work ?
+    ) or
+    (
+      "vcsn-z-fmp-tdc" == type and
+      file !~ /backward_realtime.hh/ and	# should work ?
+      file !~ /complement.hh/ and
+      file !~ /complete.hh/ and
+      file !~ /composition_cover.hh/ and	# should work ?
+      file !~ /determinize.hh/ and
+      file !~ /extension.hh/ and
+      file !~ /forward_realtime.hh/ and	 	# should work ?
+      file !~ /is_letterized.hh/ and
+      file !~ /is_normalized.hh/ and
+      file !~ /minimization_hopcroft.hh/ and
+      file !~ /minimization_moore.hh/ and
+      file !~ /normalized_composition.hh/ and
+      file !~ /realtime.hh/ and			# should work ?
+      file !~ /realtime_decl.hh/		# should work ?
+    ) or
+    (
+      "vcsn-z-tdc" == type and
+      file !~ /aut_to_exp.hh/ and
+      file !~ /backward_realtime.hh/ and	# should work ?
+      file !~ /composition_cover.hh/ and	# should work ?
+      file !~ /extension.hh/ and
+      file !~ /invert.hh/ and			# should work ?
+      file !~ /is_letterized.hh/ and
+      file !~ /is_normalized.hh/ and
+      file !~ /minimization_hopcroft.hh/ and
+      file !~ /normalized_composition.hh/ and
+      file !~ /realtime_decl.hh/ and		# should work ?
+      file !~ /standard.hh/
+    )
+  )
+end
+
+# write a Makefile for `type`
+def write_makefile(type)
+  File.open(type + "/Makefile.am.tmp", "w") { |out|
+    out.puts("## Vaucanson, a generic library for finite state machines.",
+	     "## Copyright (C) " + Time.now.strftime("%Y") + " The Vaucanson Group.",
+	     "##",
+	     "## This program is free software; you can redistribute it and/or",
+	     "## modify it under the terms of the GNU General Public License",
+	     "## as published by the Free Software Foundation; either version 2",
+	     "## of the License, or (at your option) any later version.",
+	     "##",
+	     "## The complete GNU General Public Licence Notice can be found as the",
+	     "## `COPYING' file in the root directory.",
+	     "##",
+	     "## The Vaucanson Group consists of people listed in the `AUTHORS' file.",
+	     "##",
+	     "",
+	     "INCLUDES\t\t=  -I$(top_builddir)/include -I$(top_srcdir)/include -I.",
+	     "noinst_LTLIBRARIES\t= ../lib" + type + ".la",
+	     "___lib" + type.gsub(/-/, "_") + "_la_LIBADD\t= @LIBOBJS@")
+    out.print "___lib" + type.gsub(/-/, "_") + "_la_SOURCES\t= "
+    files = Dir.glob(type + "/*cc").sort!
+    files.each { |filename| out.print "\\\n\t\t\t", File.basename(filename), "\t" }
+    out.print "\n\n"
+    out.puts("distclean-local:", "\trm -f *.cc")
+  }
+end
+
+# write the output file `type`/`fname` with `contents`
+# headers are written according to the `type`
+def write_src(type, fname, contents)
+  puts "Generating " + type + "/" + fname
+  File.open(type + '/' + fname, "w") { |out|
+    out.puts("// " + fname + ": this file is part of the Vaucanson project.",
+	     "//",
+	     "// Vaucanson, a generic library for finite state machines.",
+	     "//",
+	     "// Copyright (C) " + Time.now.strftime("%Y") + " The Vaucanson Group.",
+	     "//",
+	     "// This program is free software; you can redistribute it and/or",
+	     "// modify it under the terms of the GNU General Public License",
+	     "// as published by the Free Software Foundation; either version 2",
+	     "// of the License, or (at your option) any later version.",
+	     "//",
+	     "// The complete GNU General Public Licence Notice can be found as the",
+	     "// `COPYING' file in the root directory.",
+	     "//",
+	     "// The Vaucanson Group consists of people listed in the `AUTHORS' file.",
+	     "//",
+	     "",
+	     "#include <" + type.gsub(/-/, "_") + ".hh>",
+	     "#include <vaucanson/algorithms/" + File.basename(fname, ".cc") + ".hh>",
+	     "")
+    out.puts(contents)
+  }
+end
+
 # retrieve an array with all the headers contained
 # in ./include/vaucanson/algorithms
 files = Dir.glob("../include/vaucanson/algorithms/*.hh")
 
-# get current time (needed for headers)
-t = Time.now
-
 # read the directory
 files.each { |file|
+#  if create?("vcsn-b", file)
   empty = true
-  # checking for last update
-  if ( ! File.exist?("vcsn-b/" + File.basename(file, ".hh") + ".cc") or
-       File.new(file, "r").stat > File.new("vcsn-b/" + File.basename(file, ".hh") + ".cc").stat ) and
-       # FIXME list of ignored files shall be removed later
-       file !~ /invert.hh/ and			# transducer
-       file !~ /composition_cover.hh/ and	# transducer
-       file !~ /backward_realtime.hh/ and	# (co)accessible are missing? seems to be related to realtime.hh
-       file !~ /normalized_composition.hh/ and	# transducer
-       file !~ /is_letterized.hh/ and		# transducer
-       file !~ /is_normalized.hh/ and		# transducer
-       file !~ /realtime_decl.hh/ and		# do_(is_)realtime are missing?
-       file !~ /krat_exp_linearize.hh/ and	# weird parsing which shall be done soon
-       file !~ /extension.hh/			# transducer
-    File.open(file, "r") {|f|
-      # open the output file
-      File.open("vcsn-b/" + File.basename(file, ".hh") + ".cc", "w") { |out|
-	contents = f.readlines
+  File.open(file, "r") { |f|
+    contents = f.readlines
+    output = Array.new
+    empty = true
 
-	# print the header
-	out.print "// ", File.basename(file, ".hh"), ".cc: this file is part of the Vaucanson project.\n"
-	out.print "//\n"
-	out.print "// Vaucanson, a generic library for finite state machines.\n"
-	out.print "//\n"
-	out.print "// Copyright (C) ", t.year, " The Vaucanson Group.\n"
-	out.print "//\n"
-	out.print "// This program is free software; you can redistribute it and/or\n"
-	out.print "// modify it under the terms of the GNU General Public License\n"
-	out.print "// as published by the Free Software Foundation; either version 2\n"
-	out.print "// of the License, or (at your option) any later version.\n"
-	out.print "//\n"
-	out.print "// The complete GNU General Public Licence Notice can be found as the\n"
-	out.print "// `COPYING' file in the root directory.\n"
-	out.print "//\n"
-	out.print "// The Vaucanson Group consists of people listed in the `AUTHORS' file.\n"
-	out.print "//\n\n"
-	out.print "#define VCSN_CONTEXT_NAMESPACE namespace boolean_automaton\n"
-	out.print "#include <vaucanson/contexts/char_letter.thh>\n"
-	out.print "#include <vaucanson/contexts/dynamic_alphabet.thh>\n"
-	out.print "#include <vaucanson/contexts/free_monoid.thh>\n"
-	out.print "#include <vaucanson/contexts/boolean_semiring.thh>\n"
-	out.print "#include <vaucanson/contexts/generic_series.thh>\n"
-	out.print "#include <vaucanson/contexts/generic_automaton_impl.thh>\n"
-	out.print "#include <vaucanson/contexts/automaton.thh>\n"
-	out.print "#include <vaucanson/algorithms/", File.basename(file), ">\n\n"
-	out.print "namespace vcsn\n{\n"
-	out.print "  VCSN_CONTEXT_NAMESPACE\n  {\n"
-	out.print "    typedef vcsn::Automata<series_set_elt_t::set_t>	       automata_set_t;\n"
-	out.print "    typedef vcsn::Element<automata_set_t, automaton_impl_t>    automaton_t;\n"
-	out.print "    typedef vcsn::generalized_traits<automaton_t>::automaton_t gen_automaton_t;\n"
-	out.print "  }\n\n"
-
-	# read the file
-	i = 0
-	while i < contents.length
-	  # template has been found
-	  # process the file declaration
-	  if contents[i] =~ /template\s*<typename\s*[A-Z]*,\s*typename\s*[A-Z]*>/
-	    empty = false
-	    out.print contents[i].gsub(/\s*<.*>/, "")
-	    while i < contents.length and contents[i] !~ />/
-	      i = i.next
-	    end
-	    i = i.next
-	    while i < contents.length
-	      out.print "", contents[i].gsub(/(typename\s*)*Element<\w*,\s*\w*>/, "boolean_automaton::automaton_t")
-	      if contents[i] =~ /;/
-		out.print "\n"
-		break
-	      end
-	      i = i.next
-	    end
-	  elsif ( contents[i] =~ / namespace/ or contents[i] =~ /^\s+\}/ ) and
-		( contents[i] !~ /vcsn/ and contents[i] !~ /\};/ )
-	    out.puts contents[i]
-	  end
-
+    i = 0
+    while i < contents.length
+      # template has been found
+      # process the file declaration
+      if contents[i] =~ /template\s*<typename\s*[A-Z]*,\s*typename\s*[A-Z]*>/
+	empty = false
+	output.push(contents[i].gsub(/\s*<.*>/, ""))
+	while i < contents.length and contents[i] !~ />/
 	  i = i.next
 	end
-	out.print "}\n"
-      }
-    }
-    # checking if there was any declaration kept from the current header
-    if empty
-      File.delete("vcsn-b/" + File.basename(file, ".hh") + ".cc")
-    else
-      print "Generating vcsn-b/", File.basename(file, ".hh"), ".cc\n"
+	i = i.next
+	while i < contents.length
+	  output.push(contents[i].gsub(/(typename\s*)*Element<\w*,\s*\w*>/, "VCSN_CONTEXT::automaton_t"))
+	  if contents[i] =~ /;/
+	    output.push("")
+	    break
+	  end
+	  i = i.next
+	end
+      elsif ( contents[i] =~ /namespace/ or contents[i] =~ /(\{|\})/ ) and
+	    ( contents[i] !~ /(\};|\*)/ )
+	output.push(contents[i])
+      end
+
+      i = i.next
     end
-  end
+
+    vcsn.each { |type|
+      if create?(type, file) and ! empty
+	# file has template to instanciate, write it down!
+	# checking if there was any declaration kept from the current header
+	write_src(type, File.basename(file, ".hh") + ".cc", output)
+      end
+    }
+  }
 }
 
-# Generating the new Makefile.am
-File.open("vcsn-b/Makefile.am.tmp", "w") { |out|
-  out.puts("INCLUDES\t\t=  -I$(top_builddir)/include -I$(top_srcdir)/include",
-	   "noinst_LIBRARIES\t= libvcsn-b.a",
-	   "libvcsn_b_a_LIBADD\t= @LIBOBJS@")
-  out.print "libvcsn_b_a_SOURCES\t=\t\t"
-  files = Dir.glob("vcsn-b/*cc").sort!
-  files.each { |filename| out.print "\\\n\t\t\t", File.basename(filename), "\t" }
-  out.print "\n\n"
-  out.puts("distclean-local:", "\trm -f *.cc")
-}
-
-# Checking if the new Makefile.am is really needed e.g. some modification has to be done
-if File.exist?("vcsn-b/Makefile.am")
-  `diff vcsn-b/Makefile.am vcsn-b/Makefile.am.tmp`
-  if $? != 0
-    print "Generating vcsn-b/Makefile.am\n"
-    `mv vcsn-b/Makefile.am.tmp vcsn-b/Makefile.am`
+# writing Makefiles
+vcsn.each { |type|
+  write_makefile(type)
+  if File.exist?(type + "/Makefile.am")
+    system("diff " + type + "/Makefile.am " + type + "/Makefile.am.tmp > /dev/null")
+    if $? != 0
+      puts "Generating " + type + "/Makefile.am"
+      system("mv -f " + type + "/Makefile.am.tmp " + type + "/Makefile.am")
+    else
+      File.delete(type + "/Makefile.am.tmp")
+    end
   else
-    File.delete("vcsn-b/Makefile.am.tmp")
+    puts "Generating " + type + "/Makefile.am"
+    system("mv " + type + "/Makefile.am.tmp " + type + "/Makefile.am")
   end
-else
-  print "Generating vcsn-b/Makefile.am\n"
-  `mv vcsn-b/Makefile.am.tmp vcsn-b/Makefile.am`
-end
+}
---
+++ lib/vcsn-fmp-tdc/vcsn_fmp_tdc.hh
@@ -0,0 +1,53 @@
+// vcsn_fmp_tdc.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2007 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_FMP_TDC_HH_
+# define VCSN_FMP_TDC_HH_
+
+# include "../vcsn-b/vcsn_b.hh"
+# undef VCSN_CONTEXT
+
+# define AUTOMATON_CONTEXT boolean_automaton
+# define VCSN_CONTEXT fmp_transducer
+# define VCSN_CONTEXT_NAMESPACE namespace VCSN_CONTEXT
+
+namespace vcsn
+{
+  VCSN_CONTEXT_NAMESPACE
+  {
+    typedef AUTOMATON_CONTEXT::monoid_elt_t	first_monoid_elt_t;
+    typedef first_monoid_elt_t::set_t		first_monoid_t;
+    typedef first_monoid_elt_t::value_t		first_monoid_elt_value_t;
+    typedef first_monoid_t::alphabet_t		first_alphabet_t;
+
+    typedef AUTOMATON_CONTEXT::monoid_elt_t	second_monoid_elt_t;
+    typedef second_monoid_elt_t::set_t		second_monoid_t;
+    typedef second_monoid_elt_t::value_t	second_monoid_elt_value_t;
+    typedef second_monoid_t::alphabet_t		second_alphabet_t;
+  } // End of namespace VCSN_CONTEXT_NAMESPACE.
+} // End of namespace vcsn
+
+# include <vaucanson/contexts/free_monoid_product.thh>
+# include <vaucanson/contexts/boolean_semiring.thh>
+# include <vaucanson/contexts/polynom_series.thh>
+# include <vaucanson/contexts/generic_automaton_impl.thh>
+# include <vaucanson/contexts/automaton.thh>
+
+# undef AUTOMATON_CONTEXT
+# undef VCSN_CONTEXT_NAMESPACE
+
+#endif // !VCSN_FMP_TDC_HH_ //
