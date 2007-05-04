382c48698437dfd90c624afa4820ce44c1d3d4bd
Jimmy Ma <jimmy.ma@lrde.epita.fr>
2007-05-04 18:02:18 +0000

    Add a set of functor to hash the label. 	* include/vaucanson/misc/hash_label.hh: New. 	* include/vaucanson/misc/hash_label.hxx: New.

---
+++ include/vaucanson/misc/hash_label.hh
@@ -0,0 +1,50 @@
+// hash_label.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2007 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_MISC_HASH_LABEL_HH_
+# define VCSN_MISC_HASH_LABEL_HH_
+
+# include <cstddef>
+# include <vaucanson/algebra/implementation/series/polynoms.hh>
+# include <vaucanson/algebra/implementation/series/rat/exp.hh>
+
+namespace vcsn
+{
+  namespace misc
+  {
+    template <typename T>
+    struct hash_label;
+
+    template <typename Word, typename Weight>
+    struct hash_label<algebra::polynom<Word, Weight> >
+    {
+      std::size_t operator() (const algebra::polynom<Word, Weight>& l) const;
+    };
+
+    template <typename Word, typename Weight>
+    struct hash_label<rat::exp<Word, Weight> >
+    {
+      std::size_t operator() (const rat::exp<Word, Weight>& l) const;
+    };
+  }
+} // End of namespace vcsn
+
+# if !defined VCSN_USE_INTERFACE_ONLY || defined VCSN_USE_LIB
+#  include <vaucanson/misc/hash_label.hxx>
+# endif // ! VCSN_USE_INTERFACE_ONLY || VCSN_USE_LIB
+
+#endif // ! VCSN_MISC_HASH_LABEL_HH_ //
---
+++ include/vaucanson/misc/hash_label.hxx
@@ -0,0 +1,45 @@
+// hash_label.hxx: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2007 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_MISC_HASH_LABEL_HXX_
+# define VCSN_MISC_HASH_LABEL_HXX_
+
+# include <boost/functional/hash/hash.hpp>
+
+namespace vcsn
+{
+  namespace misc
+  {
+    template <typename Word, typename Weight>
+    std::size_t
+    hash_label<algebra::polynom<Word, Weight> >::operator() (const algebra::polynom<Word, Weight>& l) const
+    {
+      std::size_t seed (0);
+
+      for (typename algebra::polynom<Word, Weight>::const_iterator i = l.begin ();
+	   i != l.end ();
+	   i++)
+      {
+	::boost::hash_range (seed, i->first.begin(), i->first.end()); // std::string
+	::boost::hash_combine (seed, i->second);
+      }
+      return seed;
+    }
+  }
+}
+
+#endif // ! VCSN_MISC_HASH_LABEL_HXX_ //
