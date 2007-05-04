570397350e84bb68412f23b49244906b9532e56d
Jimmy Ma <jimmy.ma@lrde.epita.fr>
2007-05-04 17:59:07 +0000

    Add SmartLabelContainer. 	* include/vaucanson/automata/concept/smart_label.hh: New. 	* include/vaucanson/automata/concept/smart_label.hxx: New.

---
+++ include/vaucanson/automata/concept/smart_label.hh
@@ -0,0 +1,92 @@
+// smart_label.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2007 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_AUTOMATA_CONCEPT_SMART_LABEL_HH_
+# define VCSN_AUTOMATA_CONCEPT_SMART_LABEL_HH_
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+
+# include <vaucanson/automata/concept/handlers.hh>
+# include <vaucanson/misc/hash_label.hh>
+
+namespace vcsn
+{
+  template <typename T> // T = Graph::label_t
+  class SmartLabel
+  {
+    public:
+      typedef T value_type;
+
+      SmartLabel(const T& l) : value_(l), ref_(1)
+      { }
+
+      T& value () const;
+
+      // Handle the reference count
+      int ref () const;
+      int ref_dec ();
+      int ref_inc ();
+
+   private:
+      T value_;
+      int ref_;
+  };
+
+  template <typename T> // T = Graph::label_t
+  class SmartLabelContainer
+  {
+    public:
+      typedef T value_type;
+
+    private:
+      struct label {};
+      typedef ::boost::multi_index_container<
+	  SmartLabel<T>,
+	  ::boost::multi_index::indexed_by<
+	    ::boost::multi_index::hashed_unique<
+	      ::boost::multi_index::tag<label>,
+	      BOOST_MULTI_INDEX_CONST_MEM_FUN (SmartLabel<T>, T&, value),
+	      misc::hash_label<T>
+	    >
+	  >
+	> label_container_t;
+
+      label_container_t data_;
+
+    public:
+      struct label_h
+      {};
+      typedef handler<label_h, const SmartLabel<T>*> hlabel_t;
+
+      hlabel_t insert (const T&);
+      void erase (hlabel_t);
+      hlabel_t update (hlabel_t);
+
+      T get_label (hlabel_t) const;
+      hlabel_t get_hlabel (T) const;
+  };
+
+} // End of namespace vcsn
+
+# if !defined VCSN_USE_INTERFACE_ONLY || defined VCSN_USE_LIB
+#  include <vaucanson/automata/concept/smart_label.hxx>
+# endif // !VCSN_USE_INTERFACE_ONLY || VCSN_USE_LIB
+
+#endif // !VCSN_AUTOMATA_CONCEPT_SMART_LABEL_HH_ //
---
+++ include/vaucanson/automata/concept/smart_label.hxx
@@ -0,0 +1,61 @@
+// smart_label.hxx: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2007 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_AUTOMATA_CONCEPT_SMART_LABEL_HXX_
+# define VCSN_AUTOMATA_CONCEPT_SMART_LABEL_HXX_
+
+namespace vcsn
+{
+
+  template <typename T>
+  struct SmartLabel_ref_inc
+  {
+      SmartLabel_ref_inc(const SmartLabel<T>&)
+      { }
+
+      int operator() (SmartLabel<T>& sl)
+      {
+	return sl.ref_inc ();
+      }
+
+    private:
+  };
+
+
+  template <typename T>
+  typename SmartLabelContainer<T>::hlabel_t
+  SmartLabelContainer<T>::insert (const T& l)
+  {
+    typename label_container_t::iterator i = data_.find(l);
+
+    if (i != data_.end())
+    {
+      data_.modify (i, SmartLabel_ref_inc<T> (*i));
+      return (hlabel_t(&*i));
+    }
+    /*
+    else
+    {
+      // FIXME
+      SmartLabel<T> sl(l);
+      return hlabel_t(&sl);
+    }*/
+  }
+
+} // End of namespace vcsn
+
+#endif // ! VCSN_AUTOMATA_CONCEPT_SMART_LABEL_HXX_ //
