9d663e340dac0bce8749a2d72d49b8fca439c3e1
Vivien Delmon <vivien.delmon@lrde.epita.fr>
2008-06-19 17:00:41 +0000

    Remove useless LDADD.
    	* data/automata/b/Makefile.am: .
    	* data/automata/tdc/Makefile.am: .
    	* data/automata/z/Makefile.am: .
    	* src/tests/xml/Makefile.am: .
    	* lib/krat_exp/libkrat_exp.mk: .

--- lib/krat_exp/libkrat_exp.mk
+++ lib/krat_exp/libkrat_exp.mk
@@ -11,18 +11,11 @@
 ##
 ## The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
-EXTRA_DIST= krat_exp/krat_exp_bison.tab.hh \
-	    krat_exp/stack.hh \
-	    krat_exp/location.hh \
-	    krat_exp/position.hh
-
 lib_LTLIBRARIES	+= libkrat_exp.la
 libkrat_exp_la_CXXFLAGS	= $(CXXFLAGS) -I$(top_srcdir)/lib/krat_exp
 libkrat_exp_la_LIBADD	= $(LIBOBJS)
-libkrat_exp_la_SOURCES	= krat_exp/krat_exp_bison.tab.cc
-
-MAINTAINERCLEANFILES += $(libkrat_exp_la_SOURCES) \
-			krat_exp/krat_exp_bison.tab.hh \
-			krat_exp/stack.hh \
-			krat_exp/location.hh \
-			krat_exp/position.hh
+libkrat_exp_la_SOURCES	= krat_exp/krat_exp_bison.tab.cc \
+			  krat_exp/krat_exp_bison.tab.hh \
+			  krat_exp/stack.hh \
+			  krat_exp/location.hh \
+			  krat_exp/position.hh
