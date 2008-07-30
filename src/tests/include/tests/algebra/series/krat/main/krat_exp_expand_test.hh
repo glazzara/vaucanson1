b828065500bbfd192f26c6b5fbf79d2c8202547b
Vivien Delmon <vivien.delmon@lrde.epita.fr>
2008-07-30 15:32:10 +0000

    Fix tapping mistake in krat_exp_expand_test.
    	* src/tests/include/tests/algebra/series/krat/main/krat_exp_expand_test.hh:
    	    Remove ')'.

--- src/tests/include/tests/algebra/series/krat/main/krat_exp_expand_test.hh
+++ src/tests/include/tests/algebra/series/krat/main/krat_exp_expand_test.hh
@@ -1,17 +1,17 @@
 // krat_exp_expand_test.hh: this file is part of the Vaucanson project.
-// 
+//
 // Vaucanson, a generic library for finite state machines.
-// 
+//
 // Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006 The Vaucanson Group.
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // The complete GNU General Public Licence Notice can be found as the
 // `COPYING' file in the root directory.
-// 
+//
 // The Vaucanson Group consists of people listed in the `AUTHORS' file.
 //
 #ifndef VCSN_TESTS_ALGEBRA_SERIES_KRAT_MAIN_KRAT_EXP_EXPAND_TEST_HH
@@ -54,7 +54,7 @@ struct specialized_tests
     test(t, e, "({2} a)+a", "({3} a)");
     test(t, e, "a+({2} a)", "({3} a)");
     test(t, e, "(a+({2} a+b))", "(({3} a)+b)");
-    test(t, e, "((a+b)+a))", "(({2} a)+b)");
+    test(t, e, "((a+b)+a)", "(({2} a)+b)");
     test(t, e, "{3} b.(a+{2} b)", "(({3} ba)+({6} bb))");
     test(t, e, "(a+{2} b).({4} a)", "(({4} aa)+({8} ba))");
     test(t, e, "(a+{2} b).(a {4})", "(({4} aa)+({8} ba))");
