4aa0bbb8f5a97f297990f11383b5fa89d2563f96
Jerome Galtier <jerome.galtier@lrde.epita.fr>
2008-10-06 09:46:16 +0000

    Make FMP transducers headers more in line with RW transducers.
    	* include/Makefile.am: Add new file to the distribution.
    	* include/vaucanson/automata/generic_contexts/fmp_transducer_structures.hh,
    	* include/vaucanson/automata/generic_contexts/int_fmp_transducer_structures.hh,
    	* include/vaucanson/automata/generic_contexts/int_z_fmp_transducer_structures.hh,
    	* include/vaucanson/automata/generic_contexts/z_fmp_transducer_structures.hh:
    	  Include the new header (transducer.thh).
    	* include/vaucanson/contexts/fmp_transducer.thh: Copy from
    	  include/vaucanson/contexts/transducer.thh. Remove useless code.

--- include/vaucanson/automata/generic_contexts/int_fmp_transducer_structures.hh
+++ include/vaucanson/automata/generic_contexts/int_fmp_transducer_structures.hh
@@ -2,7 +2,7 @@
 //
 // Vaucanson, a generic library for finite state machines.
 //
-// Copyright (C) 2007 The Vaucanson Group.
+// Copyright (C) 2007, 2008 The Vaucanson Group.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -31,15 +31,17 @@ namespace vcsn
   {
     VCSN_CONTEXT_NAMESPACE
     {
+
       typedef AUTOMATON_CONTEXT::monoid_elt_t	first_monoid_elt_t;
       typedef first_monoid_elt_t::set_t		first_monoid_t;
-      typedef first_monoid_elt_t::value_t		first_monoid_elt_value_t;
-      typedef first_monoid_t::alphabet_t		first_alphabet_t;
+      typedef first_monoid_elt_t::value_t	first_monoid_elt_value_t;
+      typedef first_monoid_t::alphabet_t	first_alphabet_t;
 
       typedef AUTOMATON_CONTEXT::monoid_elt_t	second_monoid_elt_t;
-      typedef second_monoid_elt_t::set_t		second_monoid_t;
+      typedef second_monoid_elt_t::set_t	second_monoid_t;
       typedef second_monoid_elt_t::value_t	second_monoid_elt_value_t;
-      typedef second_monoid_t::alphabet_t		second_alphabet_t;
+      typedef second_monoid_t::alphabet_t	second_alphabet_t;
+
     } // End of namespace VCSN_CONTEXT_NAMESPACE.
   } // End of namespace VCSN_GRAPH_IMPL
 } // End of namespace vcsn
@@ -49,6 +51,7 @@ namespace vcsn
 # include <vaucanson/contexts/polynom_series.thh>
 # include <vaucanson/contexts/generic_automaton_impl.thh>
 # include <vaucanson/contexts/automaton.thh>
+# include <vaucanson/contexts/fmp_transducer.thh>
 
 # include <vaucanson/contexts/fmp_transducer_maker.thh>
 
