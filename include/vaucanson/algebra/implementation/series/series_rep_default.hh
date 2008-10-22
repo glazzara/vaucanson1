3c2d44d0de00f41a3cc26d0dd1caf9c28bd487d6
Vivien Delmon <vivien.delmon@lrde.epita.fr>
2008-10-22 09:19:21 +0000

    New structure to handle representation of series.
    	* series/series_rep_default.hh: New
    	  Add a singleton (series_rep_default) that handles a boost_shared pointer to
    	  handle the series representation (series_rep).
    	* series/series_rep_default.hxx: New
    	  Add the default representation of a series.

---
+++ include/vaucanson/algebra/implementation/series/series_rep_default.hh
@@ -0,0 +1,64 @@
+// series_rep_default.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2008 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_ALGEBRA_IMPLEMENTATION_SERIES_SERIES_REP_DEFAULT_HH_
+# define VCSN_ALGEBRA_IMPLEMENTATION_SERIES_SERIES_REP_DEFAULT_HH_
+# include <boost/shared_ptr.hpp>
+# include <vector>
+
+namespace vcsn {
+
+  namespace algebra {
+
+    template <typename Semiring, typename Monoid>
+    struct series_rep
+    {
+      std::string		open_par;
+      std::string		close_par;
+      std::string		plus;
+      std::string		times;
+      std::string		star;
+      std::string		zero;
+      std::string		open_weight;
+      std::string		close_weight;
+      std::vector<std::string>	spaces;
+
+      // Default CTOR
+      series_rep();
+    };
+
+    template <typename Semiring, typename Monoid>
+    struct series_rep_default
+    {
+      static boost::shared_ptr<series_rep<Semiring, Monoid> > get_instance()
+      {
+	static boost::shared_ptr<series_rep<Semiring, Monoid> > p;
+	if (p == 0)
+	  p = new series_rep<Semiring, Monoid>;
+	return p;
+      }
+    };
+
+  } // ! algebra
+
+} // ! vcsn
+
+# if !defined VCSN_USE_INTERFACE_ONLY || defined VCSN_USE_LIB
+#  include <vaucanson/algebra/implementation/series/series_rep_default.hxx>
+# endif // VCSN_USE_INTERFACE_ONLY
+
+#endif // !VCSN_ALGEBRA_IMPLEMENTATION_SERIES_SERIES_REP_DEFAULT_HH_ //
---
+++ include/vaucanson/algebra/implementation/series/series_rep_default.hxx
@@ -0,0 +1,42 @@
+// series_rep_default.hxx: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2008 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_ALGEBRA_IMPLEMENTATION_SERIES_SERIES_REP_DEFAULT_HXX_
+# define VCSN_ALGEBRA_IMPLEMENTATION_SERIES_SERIES_REP_DEFAULT_HXX_
+
+namespace vcsn {
+
+  namespace algebra {
+
+    template <typename Semiring, typename Monoid>
+    series_rep<Semiring, Monoid>::series_rep() :
+      open_par("("),
+      close_par(")"),
+      plus("+"),
+      times("."),
+      star("*"),
+      zero("null"),
+      open_weight("{"),
+      close_weight("}")
+    {
+      spaces.push_back(" ");
+    }
+
+  } // ! algebra
+
+} // ! vcsn
+#endif // !VCSN_ALGEBRA_IMPLEMENTATION_SERIES_SERIES_REP_DEFAULT_HXX_ //
