9fb6346709ad6fa5548ce31e68762e457dbbda4c
Vivien Delmon <vivien.delmon@lrde.epita.fr>
2008-10-22 09:23:31 +0000

    New structure to handle representation of monoid.
    	* monoid: New directory to restructure monoid.
    	* monoid/monoid_rep.hh: New
    	  Add a singleton (monoid_rep_default) that handles a boost_shared
    	  pointer which handles the monoid representation (monoid_rep).
    	* monoid/monoid_rep.hxx: New
    	  Add the default representation of a monoid.

---
+++ include/vaucanson/algebra/implementation/monoid/monoid_rep.hh
@@ -0,0 +1,55 @@
+// freemonoid_rep_default.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2008 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_ALGEBRA_IMPLEMENTATION_MONOID_MONOID_REP_DEFAULT_HH_
+# define VCSN_ALGEBRA_IMPLEMENTATION_MONOID_MONOID_REP_DEFAULT_HH_
+# include <boost/shared_ptr.hpp>
+
+namespace vcsn {
+
+  namespace algebra {
+
+    template <typename Monoid>
+    struct monoid_rep
+    {
+      std::string	empty;
+
+      // Default CTOR
+      monoid_rep();
+    };
+
+    template <typename Monoid>
+    struct monoid_rep_default
+    {
+      static boost::shared_ptr<monoid_rep<Monoid> > get_instance()
+      {
+	static boost::shared_ptr<monoid_rep<Monoid> > p;
+	if (p == 0)
+	  p = new monoid_rep<Monoid>;
+	return p;
+      }
+    };
+
+  } // ! algebra
+
+} // ! vcsn
+
+# if !defined VCSN_USE_INTERFACE_ONLY || defined VCSN_USE_LIB
+#  include <vaucanson/algebra/implementation/monoid/monoid_rep_default.hxx>
+# endif // VCSN_USE_INTERFACE_ONLY
+
+#endif // !VCSN_ALGEBRA_IMPLEMENTATION_MONOID_MONOID_REP_DEFAULT_HH_ //
---
+++ include/vaucanson/algebra/implementation/monoid/monoid_rep.hxx
@@ -0,0 +1,35 @@
+// freemonoid_rep_default.hxx: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2008 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+#ifndef VCSN_ALGEBRA_IMPLEMENTATION_MONOID_MONOID_REP_DEFAULT_HXX_
+# define VCSN_ALGEBRA_IMPLEMENTATION_MONOID_MONOID_REP_DEFAULT_HXX_
+
+namespace vcsn {
+
+  namespace algebra {
+
+    template <Alphabet>
+    monoid_rep<Monoid>::monoid_rep() :
+      empty(0)
+    {
+    }
+
+  } // ! algebra
+
+} // ! vcsn
+
+#endif // !VCSN_ALGEBRA_IMPLEMENTATION_MONOID_MONOID_REP_DEFAULT_HXX_ //
