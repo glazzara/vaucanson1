1c34e3423143e7889e4de6f3e81dc7e94429f33d
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2008-11-22 06:54:05 +0000

    Rename src/benchs/ as src/bench/

--- src/bench/common/bench.mk
+++ src/bench/common/bench.mk
@@ -2,8 +2,7 @@
 # $Id$
 
 AM_CXXFLAGS = $(CXXFLAGS_OPTIMIZE)
-AM_CPPFLAGS = -I$(top_builddir) \
-	      -I$(top_srcdir)/include -I$(top_srcdir)/src/benchs	\
+AM_CPPFLAGS = -I$(top_srcdir)/include -I$(top_srcdir)/src/bench	\
 	      -I$(top_builddir)/include -I$(top_builddir)/src/bench
 AM_CPPFLAGS += -DEXCEPTION_TRAPS
 AM_CPPFLAGS += -DVCSN_SRC_DIR=\"'\$(top_srcdir)'\"
