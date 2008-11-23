50859a63439e7782e56c9d82d14e84573f04c33d
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2008-11-23 07:57:52 +0000

    Use Boost's flags in src/bench/

--- src/bench/common/bench.mk
+++ src/bench/common/bench.mk
@@ -1,9 +1,8 @@
 # Makefile.flags -*- Makefile -*-
-# $Id$
 
-AM_CXXFLAGS = $(CXXFLAGS_OPTIMIZE)
-AM_CPPFLAGS = -I$(top_srcdir)/include -I$(top_srcdir)/src/bench	\
-	      -I$(top_builddir)/include -I$(top_builddir)/src/bench
+include $(top_srcdir)/vaucanson.mk
+
+AM_CPPFLAGS += -I$(top_srcdir)/src/bench -I$(top_builddir)/src/bench
 AM_CPPFLAGS += -DEXCEPTION_TRAPS
 AM_CPPFLAGS += -DVCSN_SRC_DIR=\"'\$(top_srcdir)'\"
 AM_CPPFLAGS += -DNDEBUG -O3 -DVCSN_BENCH
