9752022527e389c4ec193204f3209e8e8b638253
Jerome Galtier <jerome.galtier@lrde.epita.fr>
2009-01-22 22:00:26 +0000

    Set the correct rights in lrde-upload.sh.
    	* lrde-upload.sh: Add read permissions for everyone on
    	  `vaucanson-manual.pdf'.

--- lrde-upload.sh
+++ lrde-upload.sh
@@ -15,6 +15,7 @@ cp -pR doc/ref /lrde/dload/vaucanson/snapshot/ref.tmp
 chmod -R a+rX /lrde/dload/vaucanson/snapshot/ref.tmp
 
 cp doc/manual/vaucanson-manual.pdf /lrde/dload/vaucanson/snapshot/
+chmod a+rX /lrde/dload/vaucanson/snapshot/vaucanson-manual.pdf
 
 cp -f vaucanson-*.tar.gz /lrde/dload/vaucanson/snapshot/vaucanson-snapshot.tar.gz
 chmod a+rX /lrde/dload/vaucanson/snapshot/vaucanson-snapshot.tar.gz
