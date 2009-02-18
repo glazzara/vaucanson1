5eebc564a569619e5ecd28fab5e60102ae085077
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2009-02-18 09:54:23 +0000

    document canonical()

--- include/vaucanson/algorithms/aci_canonical.hh
+++ include/vaucanson/algorithms/aci_canonical.hh
@@ -2,7 +2,7 @@
 //
 // Vaucanson, a generic library for finite state machines.
 //
-// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2008 The Vaucanson Group.
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2008, 2009 The Vaucanson Group.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -36,11 +36,28 @@ namespace vcsn {
   /** @addtogroup algorithms */  /** @{ */
 
    /**
-   * @brief Transform a krat expression into its canonical form, following aci-rules.
+   * @brief Transform a krat expression into a canonical form
+   * using only ACI-rules.
+   *
+   * We call <b>ACI</b>-rules the following equivalences:
+   * - <b>A</b>ssociativity of the sum: <tt>(E+F)+G = E+(F+G)</tt>
+   * - <b>C</b>ommutativity of the sum: <tt>E+F = F+E</tt>
+   * - <b>I</b>dempotence of the sum: <tt>E+E = E</tt>
+   *
+   * This algorithm will take a krat expression, and rebuild
+   * it from the ground up.  Each time a sum (or a sum of sums)
+   * is found, its operands are ordered in a way that is unique
+   * and duplicate operands are removed.
+   *
+   * Note that two krat expressions can be equivalent even though
+   * their canonical forms are different.  For instance <tt>E.E*</tt>
+   * and <tt>E*.E</tt> are equivalent, but they will not be rewritten
+   * by this algorithm since they do not contain any sum.
+   *
+   * This algorithm will run in \f$ O(n\log n) \f$, where \f$n\f$ is
+   * the number of nodes of the input expression.
    *
    * @param exp The input krat expression.
-   *
-   * @bug FIXME write doc.
    */
   template <typename S, typename SI>
   Element<S, SI>
