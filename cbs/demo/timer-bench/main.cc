aa7bd0d94917735dbda4eccc5e85d5ec054a99ec
Florent D'Halluin <d-halluin@lrde.epita.fr>
2009-07-15 18:51:01 +0200

    Update cbs.
    CBS version: d0cef86a72e495219ff5e3d63d081b9baefe2cf2.
    See cbs/Changelog.

--- cbs/demo/timer-bench/main.cc
+++ cbs/demo/timer-bench/main.cc
@@ -2,15 +2,16 @@
 //
 // CBS, a C++ benchmarking suite.
 //
-// Copyright (C) 2007, 2009 Florent D'Halluin.
+// Copyright (C) 2007, 2009 The CBS Authors.
 //
 // This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// modify it under the terms of the Do What The Fuck You Want To
+// Public License (WTFPL) version 2.
 //
-// The complete GNU General Public Licence Notice can be found as the
+// The complete WTFPL license text can be found as the
 // `COPYING' file in the root directory.
+//
+// The CBS Authors consists of people listed in the `AUTHORS' file.
 //
 
 #include <iostream>
