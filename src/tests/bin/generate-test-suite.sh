f452065f95f6374e1c08df17d517b489b8a5ccb0
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2011-01-13 12:03:08 +0100

    * src/tests/bin/generate-test-suite.sh (test_dirs): Exclude names containing '#', such as emacs temporary files.

--- src/tests/bin/generate-test-suite.sh
+++ src/tests/bin/generate-test-suite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Vaucanson, a generic library for finite state machines.
-# Copyright (C) 2001, 2002, 2004, 2005, 2006, 2008 The Vaucanson Group.
+# Copyright (C) 2001, 2002, 2004, 2005, 2006, 2008, 2011 The Vaucanson Group.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -86,7 +86,7 @@ find_tests ()
       cd $test_include_root
       for d
       do
-	find $d -name \*_$suffix.hh
+	find $d -name \*_$suffix.hh -not -name '*#*'
       done
   )
 }
