deb3a2269369b2472f766a85634fdd42cedb9234
Guillaume Fiette <fiette@bosc-guerard.lrde.epita.fr>
2011-02-19 06:04:56 +0100

    * automata/generic_contexts/z2z_automaton.hh   * automata/generic_contexts/z2z_automaton_structures.hh   * contexts/z2z_semiring.thh   * z2z_automaton.hh   * z2z_automaton_structures.hh
      Creation of a new context for use of automatons using the
      Z/2Z semiring. This is a restricted use of CyclicSemiring,
      representing Z/nZ. It uses integers for now.
      Any new automaton based on CyclicSemiring can be created
      in the exact same way.

---
+++ include/vaucanson/automata/generic_contexts/z2z_automaton_structures.hh
@@ -0,0 +1,47 @@
+// r_automaton_structures.hh: this file is part of the Vaucanson project.
+//
+// Vaucanson, a generic library for finite state machines.
+//
+// Copyright (C) 2007 The Vaucanson Group.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// The Vaucanson Group consists of people listed in the `AUTHORS' file.
+//
+
+/*
+ * CPP guard should not be inserted here as
+ * VCSN_GRAPH_IMPL could be changed.
+ */
+
+# define VCSN_CONTEXT_NAMESPACE namespace z2z_automaton
+
+# include <vaucanson/contexts/char_letter.thh>
+# include <vaucanson/contexts/dynamic_alphabet.thh>
+# include <vaucanson/contexts/free_monoid.thh>
+# include <vaucanson/contexts/z2z_semiring.thh>
+# include <vaucanson/contexts/generic_series.thh>
+# include <vaucanson/contexts/generic_automaton_impl.thh>
+# include <vaucanson/contexts/automaton.thh>
+
+namespace vcsn
+{
+  namespace VCSN_GRAPH_IMPL
+  {
+    VCSN_CONTEXT_NAMESPACE
+    {
+      AUTOMATON_FREEMONOID_TYPES_EXACT(automaton_t);
+    }
+  }
+}
+
+# include <vaucanson/contexts/automaton_maker.thh>
+
+# undef VCSN_CONTEXT_NAMESPACE
+
---
+++ include/vaucanson/z2z_automaton.hh
@@ -0,0 +1,22 @@
+#ifndef VAUCANSON_Z2Z_AUTOMATON_HH
+# define VAUCANSON_Z2Z_AUTOMATON_HH
+
+# include <vaucanson/config/pconf.hh>
+# include <vaucanson/misc/usual_macros.hh>
+# include <vaucanson/contexts/bmig/z2z_automaton.hh>
+# include <vaucanson/contexts/listg/z2z_automaton.hh>
+# if !defined(ALIAS_z2z_automaton)
+#  define ALIAS_z2z_automaton
+namespace vcsn
+{
+  namespace z2z_automaton = vcsn::VCSN_DEFAULT_GRAPH_IMPL::z2z_automaton;
+}
+# endif
+# if !defined(COMMON_TRAITS)
+#  define COMMON_TRAITS
+namespace vcsn
+{
+  VCSN_MAKE_STANDARD_OF_TRAITS(vcsn::VCSN_DEFAULT_GRAPH_IMPL::Graph);
+}
+# endif
+#endif // !VAUCANSON_Z2Z_AUTOMATON_HH
