44e9db9da732aa3c21dfb222510c01b69ad1d157
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2011-06-27 17:17:54 +0200

    Call eps_removal after composition.
    * include/vaucanson/algorithms/normalized_composition.hxx
    (do_compose): Call eps_removal_here() after composing two
    Boolean transducers.

--- include/vaucanson/algorithms/normalized_composition.hxx
+++ include/vaucanson/algorithms/normalized_composition.hxx
@@ -2,7 +2,7 @@
 //
 // Vaucanson, a generic library for finite state machines.
 //
-// Copyright (C) 2005, 2006 The Vaucanson Group.
+// Copyright (C) 2005, 2006, 2011 The Vaucanson Group.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -349,6 +349,7 @@ namespace vcsn {
       compose (ret.structure(), ret.structure().series().monoid(),
 	       lhs, rhs, ret, lhs_states, rhs_states);
     compose ();
+    eps_removal_here (ret);
   }
 
   /// Unambiguous composition.
